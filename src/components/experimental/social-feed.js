3dbf323c11377ed979e480348c932b4ecf6c115d
bryan <bryan.guner@gmail.com>
2021-06-11 01:47:19 -0400

    --- Auto-Git Commit ---

---
+++ src/components/experimental/social-feed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Youtube = ({}) => {
+    return (
+        <div>
+            <div className="powr-social-feed" id="sharethis-social-feed-60c2b3f20cb9f200113df1c3" />
+        </div>
+    );
+};
+
+export default Youtube;
