94cd7c325ac3d4498c0249ef15e5dfd41f4168b4
bryan <bryan.guner@gmail.com>
2021-06-16 07:03:40 -0400

    wub

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,94 +1,31 @@
-const path = require(`path`)
-// Log out information after a build is done
+const path = require('path');
+/* Log out information after a build is done*/
 exports.onPostBuild = ({ reporter }) => {
-  reporter.info(`Your Gatsby site has been built!`)
-}
+    reporter.info('Your Gatsby site has been built!');
+};
 // Create blog pages dynamically
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
-  const result = await graphql(`
-    query {
-      allSamplePages {
-        edges {
-          node {
-            slug
-            title
-          }
-        }
-      }
-    }
-  `)
-  result.data.allSamplePages.edges.forEach(edge => {
-    createPage({
-      path: `${edge.node.slug}`,
-      component: blogPostTemplate,
-      context: {
-        title: edge.node.title,
-      },
-    })
-  })
-}
-// Async/await
-exports.createPages = async () => {
-  // do async work
-  const result = await fetchExternalData()
-}
-// Promise API
-exports.createPages = () => {
-  return new Promise((resolve, reject) => {
-    // do async work
-  })
-}
-// Callback API
-exports.createPages = (_, pluginOptions, cb) => {
-  // do async work
-  cb()
-}
-
-const path = require(`path`)
-
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
-  // Query for markdown nodes to use in creating pages.
-  // You can query for whatever data you want to create pages for e.g.
-  // products, portfolio items, landing pages, etc.
-  // Variables can be added as the second function parameter
-  return graphql(`
-    query loadPagesQuery ($limit: Int!) {
-      allMarkdownRemark(limit: $limit) {
-        edges {
-          node {
-            frontmatter {
-              slug
+    const { createPage } = actions;
+    const blogPostTemplate = path.resolve('src/templates/blog-post.js');
+    const result = await graphql(`
+        query {
+            allSamplePages {
+                edges {
+                    node {
+                        slug
+                        title
+                    }
+                }
             }
-          }
         }
-      }
-    }
-  `, { limit: 1000 }).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
-
-    // Create blog post pages.
-    result.data.allMarkdownRemark.edges.forEach(edge => {
-      createPage({
-        // Path for this page — required
-        path: `${edge.node.frontmatter.slug}`,
-        component: blogPostTemplate,
-        context: {
-          // Add optional context data to be inserted
-          // as props into the page component.
-          //
-          // The context data can also be used as
-          // arguments to the page GraphQL query.
-          //
-          // The page "path" is always available as a GraphQL
-          // argument.
-        },
-      })
-    })
-  })
-}
+    `);
+    result.data.allSamplePages.edges.forEach((edge) => {
+        createPage({
+            path: `${edge.node.slug}`,
+            component: blogPostTemplate,
+            context: {
+                title: edge.node.title
+            }
+        });
+    });
+};
