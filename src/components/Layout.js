cadcfe55fcf94f710dd632af35c147db989d91ed
Stackbit Code Editor <projects@stackbit.com>
2021-06-30 22:32:13 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -58,7 +58,7 @@ export default class Body extends React.Component {
                     <Footer {...this.props} />
                 </div>
 
- <div className="application">
+ <div >
             <Helmet>
                 <script src="https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60dcec1ece575946"></script>
                 <script>try{Typekit.load({ async: true })}catch(e){}</script>
