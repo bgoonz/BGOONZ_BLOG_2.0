9408c117c997b3e06d06c5eee5cd8063e76382b1
Stackbit Code Editor <projects@stackbit.com>
2021-06-30 22:23:21 +0000

    html.js: changed

--- src/html.js
+++ src/html.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import AddThis from './hooks/AddThis'
+
+
+
+
 export default function HTML(props) {
     return (
         <html {...props.htmlAttributes}>
