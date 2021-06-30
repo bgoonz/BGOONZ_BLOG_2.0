a74ac51a085c85e8404569d7bc6b4d1e95493267
Stackbit Code Editor <projects@stackbit.com>
2021-06-30 22:25:21 +0000

    page.js: changed

--- src/templates/page.js
+++ src/templates/page.js
@@ -17,6 +17,12 @@ export const query = graphql`
     }
 `;
 
+
+const ShareButtons = props => {
+  useScript('https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60dcec1ece575946');
+}
+
+
 export default class Page extends React.Component {
     render() {
         return (
