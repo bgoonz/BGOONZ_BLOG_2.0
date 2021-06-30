9e83fe672cb708b7f1a6f3d2b3e28358fcf081a5
Stackbit Code Editor <projects@stackbit.com>
2021-06-30 22:23:51 +0000

    html.js: changed

--- src/html.js
+++ src/html.js
@@ -3,7 +3,11 @@ import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import AddThis from './hooks/AddThis'
 
+const ShareButtons = props => {
+  useScript('https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60dcec1ece575946');
 
+
+}
 
 
 export default function HTML(props) {
