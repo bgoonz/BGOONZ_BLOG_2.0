b1c3cb9a1f2ff55c04e05925169146898650386e
Stackbit Code Editor <projects@stackbit.com>
2021-06-30 22:25:42 +0000

    page.js: changed

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import _ from 'lodash';
 import { graphql } from 'gatsby';
-import AddThis from './hooks/AddThis'
+import AddThis from './../hooks/AddThis'
 
 
 import { Layout } from '../components/index';
