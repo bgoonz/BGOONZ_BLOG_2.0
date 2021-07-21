3bd5eb9f409c6e664bcb78c3545136974f74fb8d
Bryan C Guner <bryan.guner@gmail.com>
2021-07-21 18:12:13 +0000

    update

--- static/js/main.js
+++ static/js/main.js
@@ -1,22 +1,4 @@
-// For the default version
-const algoliasearch = require('algoliasearch');
 
-// For the default version
-import algoliasearch from 'algoliasearch';
-
-// For the search only version
-import algoliasearch from 'algoliasearch/lite';
-
-const client = algoliasearch('BAGY237H21', '5f5a2d1bb109c79803ca95250ca3496c');
-const index = client.initIndex('blog2');
-index.saveObjects(objects, {
-    autoGenerateObjectIDIfNotExist: true,
-    headers: {
-      'X-Forwarded-For': '94.228.178.246'
-    }
-  }).then(({ objectIDs }) => {
-    console.log(objectIDs);
-  });
 
 window.onGatsbyInitialClientRender = function () {
     /**
