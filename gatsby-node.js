6edca2bda09938402193d1a4e45b9ef4879c1516
Bryan C Guner <bryan.guner@gmail.com>
2021-07-21 17:55:06 +0000

    hi

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,20 @@
-const path = require("path")
+// const path = require("path")
 
-exports.onCreateWebpackConfig = ({ actions, plugins, stage }) => {
-  actions.setWebpackConfig({
-    resolve: {
-      alias: {
-        "~": path.resolve(__dirname, "src"),
-        path: require.resolve("path-browserify"),
-      },
-      fallback: {
-        fs: false,
-      },
-    },
-  })
-  if (stage === "build-javascript" || stage === "develop") {
-    actions.setWebpackConfig({
-      plugins: [plugins.provide({ process: "process/browser" })],
-    })
-  }
-}
+// exports.onCreateWebpackConfig = ({ actions, plugins, stage }) => {
+//   actions.setWebpackConfig({
+//     resolve: {
+//       alias: {
+//         "~": path.resolve(__dirname, "src"),
+//         path: require.resolve("path-browserify"),
+//       },
+//       fallback: {
+//         fs: false,
+//       },
+//     },
+//   })
+//   if (stage === "build-javascript" || stage === "develop") {
+//     actions.setWebpackConfig({
+//       plugins: [plugins.provide({ process: "process/browser" })],
+//     })
+//   }
+// }
