8d37ab84e5638e53d92ceae1271f1c0ca3b58cb8
Bryan C Guner <bryan.guner@gmail.com>
2021-07-26 10:40:31 +0000

    hi

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,18 +4,18 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-const React = require('react');
-const withPrefix = require('./src/utils/withPrefix').default;
-
-exports.onRenderBody = function ({ setHeadComponents, setPostBodyComponents }) {
-    setHeadComponents([]);
-
-    setPostBodyComponents([
-        <React.Fragment>
-            <script src={withPrefix('js/plugins.js')} />
-            <script src={withPrefix('js/main.js')} />
-            <script src={withPrefix('js/page-load.js')} />
-            <script src={withPrefix('js/page-unload.js')} />
-        </React.Fragment>
-    ]);
-};
+ const React = require('react');
+ const withPrefix = require('./src/utils/withPrefix').default;
+ 
+ exports.onRenderBody = function ({ setHeadComponents, setPostBodyComponents }) {
+     setHeadComponents([]);
+ 
+     setPostBodyComponents([
+         <React.Fragment>
+             <script src={withPrefix('js/plugins.js')} />
+             <script src={withPrefix('js/main.js')} />
+             <script src={withPrefix('js/page-load.js')} />
+             <script src={withPrefix('js/page-unload.js')} />
+         </React.Fragment>
+     ]);
+ };
