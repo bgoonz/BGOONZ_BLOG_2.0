f9a160e04eab3166f1f953b03f69740be4d81a4a
Bryan C Guner <bryan.guner@gmail.com>
2021-07-26 12:06:54 +0000

    -update

---
+++ src/utils/attribute.js
@@ -0,0 +1,6 @@
+export default function attribute(name, value, condition) {
+    if (typeof condition === 'undefined') {
+        condition = true;
+    }
+    return condition ? { [name]: value } : null;
+}
