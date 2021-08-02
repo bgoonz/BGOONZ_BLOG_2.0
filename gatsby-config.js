d9d7eacbb4ee9390194e9abd6820565ae609db29
bryan <bryan.guner@gmail.com>
2021-08-02 10:34:38 -0400

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,10 @@
-const siteMetadata = require('./site-metadata.json');
+const siteMetadata = require('./site-metadata.json')
 
-siteMetadata.siteUrl = `https://bgoonz-blog.netlify.app/`;
-
-plugins: [`gatsby-plugin-sitemap`];
 module.exports = {
     pathPrefix: '/',
     siteMetadata: siteMetadata,
     plugins: [
         `gatsby-plugin-react-helmet`,
-        'gatsby-plugin-dark-mode',
         `gatsby-source-data`,
         `gatsby-transformer-remark`,
         {
@@ -30,7 +26,7 @@ module.exports = {
             resolve: `@stackbit/gatsby-plugin-menus`,
             options: {
                 sourceUrlPath: `fields.url`,
-                pageContextProperty: `menus`
+                pageContextProperty: `menus`,
             }
         }
     ]
