34a5c62f946591139e81a8132e4fe34f9efc7f93
Stackbit Code Editor <projects@stackbit.com>
2021-08-03 15:51:06 +0000

    algolia-embed.js: changed

---
+++ src/utils/algolia-embed.js
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.css" />
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js"></script>
+<script type="text/javascript">
+  algoliasearchNetlify({
+    appId: 'BAGY237H21',
+    apiKey: '5f5a2d1bb109c79803ca95250ca3496c',
+    siteId: 'a1b7ee1a-11a7-4bd2-a341-2260656e216f',
+    branch: 'master',
+    selector: 'div#search',
+  });
+</script>
