578afb095ab0606a793873bc934846c067c8568e
Stackbit Code Editor <projects@stackbit.com>
2021-08-03 15:52:26 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 
 import { htmlToReact } from '../utils';
 import ActionLink from './ActionLink';
-import 
+import algolia-embed from
 export default class Footer extends React.Component {
     render() {
         return (
