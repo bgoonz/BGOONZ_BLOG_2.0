0df6ce606d2308b7097c0f85eff4092699ed80ac
Stackbit Code Editor <projects@stackbit.com>
2021-08-03 19:17:36 +0000

    gatsby-config.js: changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,38 @@
 const siteMetadata = require('./site-metadata.json');
 require('dotenv').config({ path: `.env` })
+const query = `{
+  allSitePage {
+    edges {
+      node {
+        # try to find a unique id for each node
+        # if this field is absent, it's going to
+        # be inserted by Algolia automatically
+        # and will be less simple to update etc.
+        objectID: id
+        component
+        path
+        componentChunkName
+        jsonName
+        internal {
+          type
+          contentDigest
+          owner
+        }
+      }
+    }
+  }
+}`
 
+const queries = [
+  {
+    query,
+    transformer: ({ data }) => data.allSitePage.edges.map(({ node }) => node), // optional
+    // indexName: 'pages', // optional
+    settings: {
+      attributesToSnippet: ['path:5', 'internal'],
+    },
+  },
+]
 siteMetadata.siteUrl = `https://bgoonz-blog.netlify.app/`;
 
 plugins: [`gatsby-plugin-sitemap`];
