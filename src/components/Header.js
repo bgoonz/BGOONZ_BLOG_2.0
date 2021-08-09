3bd5d21c33da70380ac26b09d0a49d76f53f479e
Bryan C Guner <66654881+bgoonz@users.noreply.github.com>
2021-08-09 17:32:58 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,9 @@ export default class Header extends React.Component {
     render() {
         return (
             <header id="masthead" className="site-header outer">
+                                <br />
+                    <br />
+                    <br />
                 <div className="inner">
                     <div className="site-header-inside">
                         <div className="site-branding">
