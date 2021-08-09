8966e15b815ce3aef3eb5997340aaaa42b0715a2
Bryan C Guner <bryan.guner@gmail.com>
2021-08-09 15:17:56 +0000

    -update

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -2,7 +2,6 @@
 const preferDefault = (m) => (m && m.default) || m;
 
 exports.components = {
-    'component---cache-dev-404-page-js': preferDefault(require('/workspace/BGOONZ_BLOG_2.0/.cache/dev-404-page.js')),
     'component---src-templates-advanced-js': preferDefault(require('/workspace/BGOONZ_BLOG_2.0/src/templates/advanced.js')),
     'component---src-templates-blog-js': preferDefault(require('/workspace/BGOONZ_BLOG_2.0/src/templates/blog.js')),
     'component---src-templates-docs-js': preferDefault(require('/workspace/BGOONZ_BLOG_2.0/src/templates/docs.js')),
