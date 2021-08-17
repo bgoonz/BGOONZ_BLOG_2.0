8bc34c0f0b84666414d4c9d4421f7d2c49fb4ade
Stackbit Code Editor <projects@stackbit.com>
2021-08-17 04:11:18 +0000

    general.js: changed

--- src/templates/general.js
+++ src/templates/general.js
@@ -13,3 +13,17 @@ export const query = graphql`
         }
     }
 `;
+
+export default class Advanced extends React.Component {
+    render() {
+        return (
+            <Layout {...this.props}>
+                {_.map(_.get(this.props, 'pageContext.frontmatter.sections', null), (section, section_idx) => {
+                    let component = _.upperFirst(_.camelCase(_.get(section, 'type', null)));
+                    let Component = components[component];
+                    return <Component key={section_idx} {...this.props} section={section} site={this.props.pageContext.site} />;
+                })}
+            </Layout>
+        );
+    }
+}
