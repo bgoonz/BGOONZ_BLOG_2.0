880f570464a4a210395f983e75b2997d048f4490
Stackbit Code Editor <projects@stackbit.com>
2021-08-17 05:54:36 +0000

    add-script.js: changed

--- src/utils/add-script.js
+++ src/utils/add-script.js
@@ -15,7 +15,6 @@ const importScript = resourceUrl=> {
   useEffect(() => {
     const script = document.createElement('script');
   script.src = "https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js";
-
     script.async = true;
     document.body.appendChild(script);
 return () => {
