09cc9c8e8a8a9e3dda93fcf88af828e27f625fb2
bryan <bryan.guner@gmail.com>
2021-08-17 10:32:06 -0400

    reset

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -52,7 +52,8 @@ export default class Body extends React.Component {
                     style={{
                         marginLeft: `auto`,
                         marginRight: `auto`,
-
+                        maxWidth: rhythm(24),
+                        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
                     }}
                 >
                     <header {...this.props}>
