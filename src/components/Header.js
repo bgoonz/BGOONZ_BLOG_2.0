a7661fbc8a295773b822091637c7d2249a65dd9f
Stackbit Code Editor <projects@stackbit.com>
2021-08-17 06:22:37 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -84,7 +84,7 @@ export default class Header extends React.Component {
                         )}
                     </div>
                 </div>
-                <div id="search" className="inner"></div>
+                <div id="search" ></div>
                 <div>
                     <a className="github-corner" href="https://github.com/bgoonz/BGOONZ_BLOG_2.0" aria-label="View source on Github">
                         <svg
