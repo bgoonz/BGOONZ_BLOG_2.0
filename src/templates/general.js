2d40d2db88e1639b882978a289ee07ac14c3cef6
Stackbit Code Editor <projects@stackbit.com>
2021-08-17 04:12:38 +0000

    general.js: changed

--- src/templates/general.js
+++ src/templates/general.js
@@ -24,6 +24,33 @@ export default class Advanced extends React.Component {
                     let Component = components[component];
                     return <Component key={section_idx} {...this.props} section={section} site={this.props.pageContext.site} />;
                 })}
+                 <article className="post post-full">
+                    <header className="post-header has-gradient outer">
+                        {_.get(this.props, 'pageContext.frontmatter.image', null) && (
+                            <div
+                                className="bg-img"
+                                style={toStyleObj("background-image: url('" + withPrefix(_.get(this.props, 'pageContext.frontmatter.image', null)) + "')")}
+                            />
+                        )}
+                        <div className="inner-sm">
+                            <div className="post-meta">
+                                <time
+                                    className="published"
+                                    dateTime={moment(_.get(this.props, 'pageContext.frontmatter.date', null)).strftime('%Y-%m-%d %H:%M')}
+                                >
+                                    {moment(_.get(this.props, 'pageContext.frontmatter.date', null)).strftime('%B %d, %Y')}
+                                </time>
+                            </div>
+                            <h1 className="post-title">{_.get(this.props, 'pageContext.frontmatter.title', null)}</h1>
+                            {_.get(this.props, 'pageContext.frontmatter.subtitle', null) && (
+                                <div className="post-subtitle">{htmlToReact(_.get(this.props, 'pageContext.frontmatter.subtitle', null))}</div>
+                            )}
+                        </div>
+                    </header>
+                    <div className="inner-md outer">
+                        <div className="post-content">{htmlToReact(_.get(this.props, 'pageContext.html', null))}</div>
+                    </div>
+                </article>
                               <div className="inner outer">
                     <div className="docs-content">
                         <DocsMenu {...this.props} page={this.props.pageContext} site={this.props.pageContext.site} />
