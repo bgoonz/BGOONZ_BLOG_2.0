0fc4d6ed3e52dfc8d33383183d2c3a22c97c3c01
Stackbit Code Editor <projects@stackbit.com>
2021-08-17 05:54:12 +0000

    add-script.js: changed

--- src/utils/add-script.js
+++ src/utils/add-script.js
@@ -8,3 +8,19 @@ return () => {
     document.body.removeChild(script);
   }
 }, []);
+
+
+
+import { useEffect } from 'react';
+const importScript = resourceUrl=> {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = resourceUrl;
+    script.async = true;
+    document.body.appendChild(script);
+return () => {
+      document.body.removeChild(script);
+    }
+  }, [resourceUrl]);
+};
+export default importScript;
