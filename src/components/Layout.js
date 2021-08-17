817f831e7780ed6fda5de8c35ec157800e665565
Stackbit Code Editor <projects@stackbit.com>
2021-08-17 06:26:12 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -52,6 +52,7 @@ export default class Body extends React.Component {
                 </Helmet>
                 <div id="page" className="site">
                     <Header {...this.props} />
+                            <div id="search" />
                     <main id="content" className="site-content">
                         {this.props.children}
                     </main>
