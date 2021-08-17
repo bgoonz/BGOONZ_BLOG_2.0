3bada8881150c9ef7820cc09410dce6c5d7245f1
bryan <bryan.guner@gmail.com>
2021-08-17 10:18:15 -0400

    hi

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -52,8 +52,7 @@ export default class Body extends React.Component {
                     style={{
                         marginLeft: `auto`,
                         marginRight: `auto`,
-                        maxWidth: rhythm(24),
-                        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
+
                     }}
                 >
                     <header {...this.props}>
