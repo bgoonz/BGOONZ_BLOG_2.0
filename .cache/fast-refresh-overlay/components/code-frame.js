e249d2dc77beee61fda40c476986218890f83a7b
bgoonz <bryan.guner@gmail.com>
2021-10-12 17:59:40 +0000

    -update

--- .cache/fast-refresh-overlay/components/code-frame.js
+++ .cache/fast-refresh-overlay/components/code-frame.js
@@ -1,28 +1,24 @@
-import React from "react"
+import React from 'react';
 
 const CodeFrame = ({ decoded }) => (
-  <pre data-gatsby-overlay="pre">
-    <code data-gatsby-overlay="pre__code">
-      {decoded
-        ? decoded.map((entry, index) => (
-            <span
-              key={`frame-${index}`}
-              data-gatsby-overlay="pre__code__span"
-              style={{
-                color: entry.fg ? `var(--color-${entry.fg})` : undefined,
-                ...(entry.decoration === `bold`
-                  ? { fontWeight: 800 }
-                  : entry.decoration === `italic`
-                  ? { fontStyle: `italic` }
-                  : undefined),
-              }}
-            >
-              {entry.content}
-            </span>
-          ))
-        : null}
-    </code>
-  </pre>
-)
+    <pre data-gatsby-overlay="pre">
+        <code data-gatsby-overlay="pre__code">
+            {decoded
+                ? decoded.map((entry, index) => (
+                      <span
+                          key={`frame-${index}`}
+                          data-gatsby-overlay="pre__code__span"
+                          style={{
+                              color: entry.fg ? `var(--color-${entry.fg})` : undefined,
+                              ...(entry.decoration === `bold` ? { fontWeight: 800 } : entry.decoration === `italic` ? { fontStyle: `italic` } : undefined)
+                          }}
+                      >
+                          {entry.content}
+                      </span>
+                  ))
+                : null}
+        </code>
+    </pre>
+);
 
-export default CodeFrame
+export default CodeFrame;
