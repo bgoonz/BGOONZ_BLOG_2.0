002241cbc426a3cf45b96f267052873bd8da2829
bgoonz <bryan.guner@gmail.com>
2021-10-12 23:23:58 +0000

    -update

--- src/components/DocsSubmenu.js
+++ src/components/DocsSubmenu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import _ from 'lodash';
 
-import {classNames, Link, withPrefix} from '../utils';
+import { classNames, Link, withPrefix } from '../utils';
 
 export default class DocsSubmenu extends React.Component {
     render() {
@@ -9,11 +9,16 @@ export default class DocsSubmenu extends React.Component {
         let page = _.get(this.props, 'page', null);
         return (
             <ul className="docs-submenu">
-              {_.map(child_pages, (child_page, child_page_idx) => (
-                <li key={child_page_idx} className={classNames('docs-menu-item', {'current': _.get(page, 'url', null) === _.get(child_page, 'url', null)})}>
-                  <Link to={withPrefix(_.get(child_page, 'url', null))}>{_.get(child_page, 'frontmatter.title', null)}</Link>
-                </li>
-              ))}
+                {_.map(child_pages, (child_page, child_page_idx) => (
+                    <li
+                        key={child_page_idx}
+                        className={classNames('docs-menu-item', {
+                            current: _.get(page, 'url', null) === _.get(child_page, 'url', null)
+                        })}
+                    >
+                        <Link to={withPrefix(_.get(child_page, 'url', null))}>{_.get(child_page, 'frontmatter.title', null)}</Link>
+                    </li>
+                ))}
             </ul>
         );
     }
--- src/components/Submenu.js
+++ src/components/Submenu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import _ from 'lodash';
 
-import {classNames} from '../utils';
+import { classNames } from '../utils';
 import ActionLink from './ActionLink';
 
 export default class Submenu extends React.Component {
@@ -9,15 +9,21 @@ export default class Submenu extends React.Component {
         let page = _.get(this.props, 'page', null);
         return (
             <ul className={_.get(this.props, 'menu_class', null)}>
-            {_.map(_.get(this.props, 'submenu', null), (action, action_idx) => {
-                let page_url = _.trim(_.get(page, 'url', null), '/');
-                let action_url = _.trim(_.get(action, 'url', null), '/');
-                return (
-                  <li key={action_idx} className={classNames('menu-item', {'current': page_url === action_url, 'menu-button': _.get(action, 'style', null) !== 'link'})}>
-                    <ActionLink {...this.props} action={action} />
-                  </li>
-                )
-            })}
+                {_.map(_.get(this.props, 'submenu', null), (action, action_idx) => {
+                    let page_url = _.trim(_.get(page, 'url', null), '/');
+                    let action_url = _.trim(_.get(action, 'url', null), '/');
+                    return (
+                        <li
+                            key={action_idx}
+                            className={classNames('menu-item', {
+                                current: page_url === action_url,
+                                'menu-button': _.get(action, 'style', null) !== 'link'
+                            })}
+                        >
+                            <ActionLink {...this.props} action={action} />
+                        </li>
+                    );
+                })}
             </ul>
         );
     }
