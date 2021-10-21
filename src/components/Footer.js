b5ebd779cfb978ba86e7980837dfa1f2c6a44570
Stackbit Code Editor <projects@stackbit.com>
2021-10-21 07:13:08 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -136,6 +136,10 @@ export default class Footer extends React.Component {
                                 </tr>
                             </tbody>
                         </table>
+                        <a href="//pdfcrowd.com/url_to_pdf/?"
+onclick="if(!this.p)href+='&url='+encodeURIComponent(location.href);this.p=1">
+Save to PDF
+</a>
                     </center>
                 </div>
 
