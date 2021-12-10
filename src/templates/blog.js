d48c8abf2bba1b4ba40e438ef13fc5c5f45b7c2f
Bryan C Guner <bryan.guner@gmail.com>
2021-12-10 18:33:10 +0000

    -update

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,84 +1,62 @@
 import React from 'react';
 import _ from 'lodash';
 import moment from 'moment-strftime';
+import {graphql} from 'gatsby';
 
-import { Layout } from '../components/index';
-import { getPageUrl, withPrefix, Link } from '../utils';
+import {Layout} from '../components/index';
+import {toStyleObj, withPrefix, getPages, Link} from '../utils';
+
+// this minimal GraphQL query ensures that when 'gatsby develop' is running,
+// any changes to content files are reflected in browser
+export const query = graphql`
+  query($url: String) {
+    sitePage(path: {eq: $url}) {
+      id
+    }
+  }
+`;
 
 export default class Blog extends React.Component {
-    renderPost(post, index, hasMoreLink, moreLinkText) {
-        const title = _.get(post, 'title');
-        const thumbImage = _.get(post, 'thumb_image');
-        const thumbImageAlt = _.get(post, 'thumb_image_alt', '');
-        const excerpt = _.get(post, 'excerpt');
-        const date = _.get(post, 'date');
-        const dateTimeAttr = moment(date).strftime('%Y-%m-%d %H:%M');
-        const formattedDate = moment(date).strftime('%B %d, %Y');
-        const postUrl = getPageUrl(post, { withPrefix: true });
-
-        return (
-            <article key={index} className="post">
-                {thumbImage && (
-                    <Link className="post-thumbnail" href={postUrl}>
-                        <img src={withPrefix(thumbImage)} alt={thumbImageAlt} />
-                    </Link>
-                )}
-                <header className="post-header">
-                    <div className="post-meta">
-                        <time className="published" dateTime={dateTimeAttr}>
-                            {formattedDate}
-                        </time>
-                    </div>
-                    <h2 className="post-title line-left">
-                        <Link href={postUrl}>{title}</Link>
-                    </h2>
-                </header>
-                {excerpt && (
-                    <React.Fragment>
-                        <p className="post-excerpt">{excerpt}</p>
-                        {hasMoreLink && moreLinkText && (
-                            <p className="read-more">
-                                <Link className="read-more-link" href={postUrl}>
-                                    {moreLinkText}
-                                </Link>
-                            </p>
-                        )}
-                    </React.Fragment>
-                )}
-            </article>
-        );
-    }
-
     render() {
-        const data = _.get(this.props, 'data');
-        const config = _.get(data, 'config');
-        const page = _.get(this.props, 'page');
-        const title = _.get(page, 'title');
-        const subtitle = _.get(page, 'subtitle');
-        const backgroundImage = _.get(page, 'image');
-        const hasMoreLink = _.get(page, 'has_more_link');
-        const moreLinkText = _.get(page, 'more_link_text');
-        const posts = _.orderBy(_.get(this.props, 'posts', []), 'date', 'desc');
-
+        let display_posts = _.orderBy(getPages(this.props.pageContext.pages, '/blog'), 'frontmatter.date', 'desc');
         return (
-            <Layout page={page} config={config}>
-                <header className="page-header has-gradient outer">
-                    {backgroundImage && (
-                        <div
-                            className="bg-img"
-                            style={{
-                                backgroundImage: `url('${withPrefix(backgroundImage)}')`
-                            }}
-                        />
-                    )}
-                    <div className="inner-sm">
-                        <h1 className="page-title">{title}</h1>
-                        {subtitle && <p className="page-subtitle">{subtitle}</p>}
-                    </div>
-                </header>
-                <div className="inner-md outer">
-                    <div className="post-feed">{_.map(posts, (post, index) => this.renderPost(post, index, hasMoreLink, moreLinkText))}</div>
+            <Layout {...this.props}>
+              <header className="page-header has-gradient outer">
+                {_.get(this.props, 'pageContext.frontmatter.image', null) && (
+                <div className="bg-img" style={toStyleObj('background-image: url(\'' + withPrefix(_.get(this.props, 'pageContext.frontmatter.image', null)) + '\')')}/>
+                )}
+                <div className="inner-sm">
+                  <h1 className="page-title">{_.get(this.props, 'pageContext.frontmatter.title', null)}</h1>
+                  {_.get(this.props, 'pageContext.frontmatter.subtitle', null) && (
+                  <p className="page-subtitle">{_.get(this.props, 'pageContext.frontmatter.subtitle', null)}</p>
+                  )}
                 </div>
+              </header>
+              <div className="inner-md outer">
+                <div className="post-feed">
+                  {_.map(display_posts, (post, post_idx) => (
+                  <article key={post_idx} className="post">
+                    {_.get(post, 'frontmatter.thumb_image', null) && (
+                    <Link className="post-thumbnail" to={withPrefix(_.get(post, 'url', null))}><img src={withPrefix(_.get(post, 'frontmatter.thumb_image', null))} alt={_.get(this.props, 'pageContext.frontmatter.thumb_image_alt', null)} /></Link>
+                    )}
+                    <header className="post-header">
+                      <div className="post-meta">
+                        <time className="published" dateTime={moment(_.get(post, 'frontmatter.date', null)).strftime('%Y-%m-%d %H:%M')}>{moment(_.get(post, 'frontmatter.date', null)).strftime('%B %d, %Y')}</time>
+                      </div>
+                      <h2 className="post-title line-left"><Link to={withPrefix(_.get(post, 'url', null))} rel="bookmark">{_.get(post, 'frontmatter.title', null)}</Link></h2>
+                    </header>
+                    {_.get(post, 'frontmatter.excerpt', null) && (<React.Fragment>
+                        <p className="post-excerpt">{_.get(post, 'frontmatter.excerpt', null)}</p>
+                        {((_.get(this.props, 'pageContext.frontmatter.has_more_link', null) === true) && _.get(this.props, 'pageContext.frontmatter.more_link_text', null)) && (
+                        <p className="read-more">
+                          <Link className="read-more-link" to={withPrefix(_.get(post, 'url', null))}>{_.get(this.props, 'pageContext.frontmatter.more_link_text', null)}</Link>
+                        </p>
+                        )}
+                    </React.Fragment>)}
+                  </article>
+                ))}
+                </div>
+              </div>
             </Layout>
         );
     }
