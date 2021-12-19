6815f2e11812aee4fe907582b19eb5b645991d32
Bryan C Guner <bryan.guner@gmail.com>
2021-12-19 17:06:08 +0000

    -update

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -24,8 +24,25 @@ exports.onRenderBody = function ({ setHeadComponents, setPostBodyComponents }) {
             <script src={withPrefix('js/google-search.js')} />
             <script src={withPrefix('js/algolia.js')} />
             <script src={withPrefix('js/prism.js')} />
-
+            <script src={withPrefix('js/page-unload.js')} />
         </React.Fragment>
     ]);
 
 };
+'js/addthis.js'
+'js/algolia.js'
+'js/analytics.js'
+'js/blm-badge.js'
+'js/blm.js'
+'js/copy-2-clip.js'
+'js/dist'
+'js/google-search.js'
+'js/inject.js'
+'js/main.js'
+'js/misc.js'
+'js/page-load.js'
+'js/page-unload.js'
+'js/plugins.js'
+'js/prism.js'
+'js/dist/clipboard.js'
+'js/dist/clipboard.min.js'
