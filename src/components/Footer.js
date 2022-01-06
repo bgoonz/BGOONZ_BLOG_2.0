721af75fe165c353d03ae9c24b6b6b4531cd8320
Bryan C Guner <66654881+bgoonz@users.noreply.github.com>
2022-01-06 06:49:39 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,11 +10,9 @@ export default class Footer extends React.Component {
     render() {
         return (
             <footer id="colophon" className="site-footer outer">
-                <div id="search"></div>
                 <div>
                     <center>
                         <br />
-            
                         <div id="search"> {Script} </div>
                         <br />
                         <table cellPadding={0} cellSpacing={0} border={0}>
@@ -76,7 +74,6 @@ export default class Footer extends React.Component {
                                                 </tbody>
                                             </table>
                                         </center>
-
                                         <form
                                             style={{
                                                 margin: '0px',
@@ -85,8 +82,7 @@ export default class Footer extends React.Component {
                                             action="https://search.freefind.com/find.html"
                                             method="get"
                                             acceptCharset="utf-8"
-                                            target="_self"
-                                        >
+                                            target="_self">
                                             <input type="hidden" name="si" defaultValue={14588965} />
                                             <input type="hidden" name="pid" defaultValue="r" />
                                             <input type="hidden" name="n" defaultValue={0} />
@@ -104,16 +100,14 @@ export default class Footer extends React.Component {
                                             fontFamily: 'Arial, Helvetica, sans-serif',
                                             fontSize: '7.5pt',
                                             paddingTop: '4px'
-                                        }}
-                                    >
+                                        }} >
                                         <a
                                             style={{
                                                 textDecoration: 'none',
                                                 color: 'transparent'
                                             }}
                                             href="https://www.freefind.com"
-                                            rel="nofollow"
-                                        >
+                                            rel="nofollow">
                                             search engine
                                         </a>
                                         <a
@@ -122,8 +116,7 @@ export default class Footer extends React.Component {
                                                 color: 'transparent'
                                             }}
                                             href="https://www.freefind.com"
-                                            rel="nofollow"
-                                        >
+                                            rel="nofollow">
                                             by
                                             <span style={{ color: 'transparent' }}>freefind</span>
                                         </a>
@@ -143,8 +136,8 @@ export default class Footer extends React.Component {
                             Save to PDF
                         </a>
                     </center>
+      <a aria-current="page" className href="/"><img src="https://d33wubrfki0l68.cloudfront.net/e5662f0d4f3e7730aea1a0faf7ff09ea20184700/6ca0b/images/dgqlkqjtmk.png" alt="webdevhub logo" /></a>
                 </div>
-
                 <div className="inner">
                     <div id="search" className="inner"></div>
                     <div className="site-footer-inside">
@@ -165,7 +158,6 @@ export default class Footer extends React.Component {
                         )}{' '}
                     </div>
                 </div>
-                <div id="search"></div>
             </footer>
         );
     }
