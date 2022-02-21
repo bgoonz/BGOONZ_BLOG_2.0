6b1dca5ca880a2c9ef0531d0ec26673c88a31289
Restyled.io <commits@restyled.io>
2022-02-21 10:08:01 +0000

    Restyled by clang-format

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 make
 import _ from 'lodash';
 import React from 'react';
-import { htmlToReact } from '../utils';
+import {htmlToReact} from '../utils';
 import ActionLink from './ActionLink';
 
 export default class Footer extends React.Component {
-    render() {
+  render() {
         return (
             <footer id="colophon" className="site-footer outer">
                 <div>
@@ -17,80 +17,75 @@ export default class Footer extends React.Component {
                             <tbody>
                                 <tr>
                                     <td
-                                        style={{
-                                            fontFamily: 'Arial, Helvetica, sans-serif',
-                                            fontSize: '7.5pt'
-                                        }}
-                                    >
-                                        <center>
-                                            <table
-                                                width="95%"
-                                                cellPadding={0}
-                                                cellSpacing={0}
-                                                border={0}
-                                                style={{
-                                                    fontFamily: 'Arial, Helvetica, sans-serif',
-                                                    fontSize: '7.5pt'
-                                                }}
-                                            >
-                                                <tbody>
-                                                    <tr>
-                                                        <td
-                                                            style={{
-                                                                fontFamily: 'Arial, Helvetica, sans-serif',
-                                                                fontSize: '7.5pt'
-                                                            }}
-                                                            align="left"
-                                                        >
-                                                            <a target="_blank" href="https://search.freefind.com/siteindex.html?si=14588965">
-                                                                index
-                                                            </a>
+        style = {
+          { fontFamily: 'Arial, Helvetica, sans-serif', fontSize: '7.5pt' }
+        } > <center>< table
+        width = "95%"
+        cellPadding = {0} cellSpacing = {0} border = {0} style = {
+          { fontFamily: 'Arial, Helvetica, sans-serif', fontSize: '7.5pt' }
+        } > <tbody><tr>< td
+        style =
+        {
+          { fontFamily: 'Arial, Helvetica, sans-serif', fontSize: '7.5pt' }
+        } align =
+            "left" >
+            <a target = "_blank" href =
+                 "https://search.freefind.com/siteindex.html?si=14588965">
+                index<
+                    /a>
+                                                        </td><
+            td
+        style =
+        {
+          { fontFamily: 'Arial, Helvetica, sans-serif', fontSize: '7.5pt' }
+        } align =
+            "center" >
+            <a target = "_blank" href =
+                 "https://search.freefind.com/find.html?si=14588965&m=0&p=0">
+                sitemap<
+                    /a>
+                                                        </td><
+            td
+        style =
+        {
+          { fontFamily: 'Arial, Helvetica, sans-serif', fontSize: '7.5pt' }
+        } align =
+            "right" >
+            <a target = "_blank" href =
+                 "https://search.freefind.com/find.html?si=14588965&pid=a">
+                advanced<
+                    /a>
                                                         </td>
-                                                        <td
-                                                            style={{
-                                                                fontFamily: 'Arial, Helvetica, sans-serif',
-                                                                fontSize: '7.5pt'
-                                                            }}
-                                                            align="center"
-                                                        >
-                                                            <a target="_blank" href="https://search.freefind.com/find.html?si=14588965&m=0&p=0">
-                                                                sitemap
-                                                            </a>
-                                                        </td>
-                                                        <td
-                                                            style={{
-                                                                fontFamily: 'Arial, Helvetica, sans-serif',
-                                                                fontSize: '7.5pt'
-                                                            }}
-                                                            align="right"
-                                                        >
-                                                            <a target="_blank" href="https://search.freefind.com/find.html?si=14588965&pid=a">
-                                                                advanced
-                                                            </a>
-                                                        </td>
-                                                    </tr>
+            </tr>
                                                 </tbody>
-                                            </table>
-                                        </center>
-                                        <form
-                                            style={{
-                                                margin: '0px',
-                                                marginTop: '2px'
-                                            }}
-                                            action="https://search.freefind.com/find.html"
-                                            method="get"
-                                            acceptCharset="utf-8"
-                                            target="_self">
-                                            <input type="hidden" name="si" defaultValue={14588965} />
+            </table>
+                                        </center>< form
+        style = {
+          { margin: '0px', marginTop: '2px' }
+        } action = "https://search.freefind.com/find.html"
+        method = "get"
+        acceptCharset = "utf-8"
+        target =
+            "_self" >
+            <input type = "hidden" name = "si" defaultValue =
+             {
+               14588965
+             } />
                                             <input type="hidden" name="pid" defaultValue="r" />
-                                            <input type="hidden" name="n" defaultValue={0} />
+            <input type = "hidden" name = "n" defaultValue =
+             {
+               0
+             } />
                                             <input type="hidden" name="_charset_" defaultValue />
-                                            <input type="hidden" name="bcd" defaultValue="÷" />
-                                            <input type="text" name="query" size={15} />
+            <input type = "hidden" name = "bcd" defaultValue = "÷" />
+            <input type = "text" name = "query" size =
+             {
+               15
+             } />
                                             <input type="submit" defaultValue="search" />
-                                        </form>
-                                    </td>
-                                </tr>
+            </form>
+                                    </td><
+            /tr>
                                 <tr>
                                     <td
                                         style={{
@@ -104,19 +99,22 @@ export default class Footer extends React.Component {
                                                 textDecoration: 'none',
                                                 color: 'transparent'
                                             }}
-                                            href="https://www.freefind.com"
-                                            rel="nofollow">
-                                            search engine
-                                        </a>
+                                            href="https:/ /
+                www.freefind.com "
+        rel =
+            "nofollow" > search engine <
+            /a>
                                         <a
                                             style={{
                                                 textDecoration: 'none',
                                                 color: 'transparent'
                                             }}
-                                            href="https://www.freefind.com"
+                                            href="https:/ /
+                www.freefind.com "
                                             rel="nofollow">
                                             by
-                                            <span style={{ color: 'transparent' }}>freefind</span>
+                                            <span style={{
+      color: 'transparent' }}>freefind</span>
                                         </a>
                                         {/* {
                                             <iframe
@@ -142,15 +140,18 @@ export default class Footer extends React.Component {
                         <p className="site-info">
                             {_.get(this.props, 'pageContext.site.siteMetadata.footer.content', null) && (
                                 <span className="copyright">{htmlToReact(_.get(this.props, 'pageContext.site.siteMetadata.footer.content', null))}</span>
-                            )}
-                            {_.map(_.get(this.props, 'pageContext.site.siteMetadata.footer.links', null), (action, action_idx) => (
-                                <ActionLink key={action_idx} {...this.props} action={action} />
+                            )
+  }
+  {_.map(_.get(this.props, 'pageContext.site.siteMetadata.footer.links', null), (action, action_idx) => (
+                                <ActionLink key={action_idx} {...this.props} action={
+      action} />
                             ))}{' '}
                         </p>
                         {_.get(this.props, 'pageContext.site.siteMetadata.footer.has_social', null) && (
                             <div className="social-links">
                                 {_.map(_.get(this.props, 'pageContext.site.siteMetadata.footer.social_links', null), (action, action_idx) => (
-                                    <ActionLink key={action_idx} {...this.props} action={action} />
+                                    <ActionLink key={action_idx} {...this.props} action={
+        action} />
                                 ))}{' '}
                             </div>
                         )}{' '}
