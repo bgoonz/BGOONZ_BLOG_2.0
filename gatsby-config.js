f7882f9b9f9b4893ca2760a2e29c3a9224de9ddf
Bryan C Guner <bryan.guner@gmail.com>
2022-02-24 00:17:21 +0000

    -update

---
+++ gatsby-config.js
@@ -0,0 +1,45 @@
+const siteMetadata = require('./site-metadata.json')
+
+module.exports = {
+    pathPrefix: '/',
+    siteMetadata: siteMetadata,
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-source-data`,
+        `gatsby-transformer-remark`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `pages`,
+                path: `${__dirname}/src/pages`
+            }
+        },
+            {
+              resolve: `gatsby-plugin-disqus`,
+              options: {
+                shortname: `webdevhub-1`,
+              }
+            },
+        {
+            resolve: `gatsby-plugin-sass`,
+            options: {}
+        },
+        {
+            resolve: `gatsby-remark-page-creator`,
+            options: {}
+        },
+        {
+            resolve: `@stackbit/gatsby-plugin-menus`,
+            options: {
+                sourceUrlPath: `fields.url`,
+                pageContextProperty: `menus`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-disqus`,
+            options: {
+                shortname: `webdevhub-1`
+            }
+        },
+    ]
+};
---
+++ gatsby-ssr.js
@@ -0,0 +1,32 @@
+/**
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/ssr-apis/
+ */
+
+const React = require("react");
+const withPrefix = require("./src/utils/withPrefix").default;
+
+exports.onRenderBody = function ({ setHeadComponents, setPostBodyComponents }) {
+
+    setHeadComponents([
+
+    ]);
+
+    setPostBodyComponents([
+        <React.Fragment>
+            <script src={withPrefix('js/plugins.js')} />
+            <script src={withPrefix('js/main.js')} />
+            <script src={withPrefix('js/page-load.js')} />
+            <script src={withPrefix('js/page-unload.js')} />
+            <script src={withPrefix('js/blm.js')} />
+            <script src={withPrefix('js/addthis.js')} />
+            <script src={withPrefix('js/google-search.js')} />
+            <script src={withPrefix('js/algolia.js')} />
+            <script src={withPrefix('js/prism.js')} />
+            <script src={withPrefix('js/analytics.js')} />
+            <script src={withPrefix('js/plugins.js')} />
+        </React.Fragment>
+    ]);
+
+};
