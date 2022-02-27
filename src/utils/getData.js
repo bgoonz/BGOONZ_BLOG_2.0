8714fc663be41aa3e8db715db1ed46475d0d3be6
Stackbit Code Editor <projects@stackbit.com>
2022-02-27 22:43:25 +0000

    getData.js: changed

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,17 +1,12 @@
 import _ from 'lodash';
 
-export default function getData(siteData, dataPath) {
+export default function getData(props, dataPath) {
     dataPath = _.trim(dataPath, '/');
-
-    if (_.startsWith(dataPath, 'src/data/')) {
-        dataPath = dataPath.replace('src/data/', '');
+    if (_.startsWith(dataPath, 'content/data/')) {
+        dataPath = dataPath.replace('content/data/', '');
     }
-
     // remove extension
-
     dataPath = dataPath.replace(/\.\w+$/, '');
-
     const path = dataPath.split('/');
-
-    return _.get(siteData, path);
+    return _.get(props, path);
 }
