263c6e49d7bec98b616145991a00108cba895916
Bryan Guner <bryan.guner.dev@gmail.com>
2022-03-07 00:23:55 -0500

    prettier

--- src/components/ActionLink.js
+++ src/components/ActionLink.js
@@ -12,7 +12,8 @@ export default class ActionLink extends React.Component {
               {...(_.get(action, 'new_window', null) ? ({target: '_blank'}) : null)}
               {...((_.get(action, 'new_window', null) || _.get(action, 'no_follow', null)) ? ({rel: (_.get(action, 'new_window', null) ? ('noopener ') : '') + (_.get(action, 'no_follow', null) ? ('nofollow') : '')}) : null)}
               className={classNames({'button': _.get(action, 'style', null) !== 'link', 'button-secondary': _.get(action, 'style', null) === 'secondary', 'button-icon': _.get(action, 'style', null) === 'icon'})}>
-              {((_.get(action, 'style', null) === 'icon') && _.get(action, 'icon_class', null)) ? (<React.Fragment>
+                { ( ( _.get( action, 'style', null ) === 'icon' ) && _.get( action, 'icon_class', null ) ) ? (
+                    <React.Fragment>
                 <Icon {...this.props} icon={_.get(action, 'icon_class', null)} />
                 <span className="screen-reader-text">{_.get(action, 'label', null)}</span>
               </React.Fragment>) : 
