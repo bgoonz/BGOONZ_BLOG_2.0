29d3b0d2a31b4513da0595014ad413dbbaa9a01a
Stackbit Code Editor <projects@stackbit.com>
2022-03-14 05:15:49 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -130,6 +130,7 @@ export default class Header extends React.Component {
                     </svg>
                 </a>
                 </div>
+                </div>
             </header >
         );
     }
