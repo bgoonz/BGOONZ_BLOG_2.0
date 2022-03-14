b7b648e601f57ddd517453a1c09c30bd4ab11e86
Stackbit Code Editor <projects@stackbit.com>
2022-03-14 05:14:33 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -81,7 +81,7 @@ export default class Header extends React.Component {
                 <div>
 
   <div className="sb-content sb-hero">
-    <div className="sb-search sb-hero">
+                        <div className="sb-search sb-hero">
       <div className="sb-input">
         <input
           className="sb-search-field"
