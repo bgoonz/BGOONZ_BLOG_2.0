01663f7358d58045ad04a6cb29384959bf911595
bryan <bryan.guner@gmail.com>
2022-03-14 00:27:43 -0400

    updates

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,14 +8,20 @@
 //<script src={withPrefix('js/analytics.js')} /> }
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
-const React = require( "react" );
-const withPrefix = require( "./src/utils/withPrefix" ).default;
-exports.onRenderBody = function ( {
-    setHeadComponents,
-    setPostBodyComponents
-} ) {
-    setHeadComponents( [
-    ] );
+/**
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/ssr-apis/
+ */
+
+const React = require("react");
+const withPrefix = require("./src/utils/withPrefix").default;
+
+exports.onRenderBody = function ({ setHeadComponents, setPostBodyComponents }) {
+
+    setHeadComponents([
+
+    ]);
     setPostBodyComponents( [ < React.Fragment >
         < script src={ withPrefix( 'js/addthis.js' ) } />
         < script src={ withPrefix( 'js/blm.js' ) } />
