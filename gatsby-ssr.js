324ddfaceb8c35d759209eeee19b0c42a618f91f
bgoonz <bryan.guner@gmail.com>
2022-03-23 06:28:12 +0000

    gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,65 +8,23 @@
 //<script src={withPrefix('js/analytics.js')} /> }
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
-const React = require('react');
-const withPrefix = require('./src/utils/withPrefix').default;
-exports.onRenderBody = function({
-    setHeadComponents,
-    setPostBodyComponents
-}) {
-    setHeadComponents([]);
-    setPostBodyComponents([ <
-        React.Fragment >
-        <
-        link rel = "stylesheet"
-        href = {
-            withPrefix('css/main.css')
-        }
-        /> <
-        script src = {
-            withPrefix('js/addthis.js')
-        }
-        /> <
-        script src = {
-            withPrefix('js/blm.js')
-        }
-        /> <
-        script src = {
-            withPrefix('js/inject.js')
-        }
-        /> <
-        script src = {
-            withPrefix('js/searchbar.js')
-        }
-        /> <
-        script src = {
-            withPrefix('js/plugins.js')
-        }
-        /> <
-        script src = {
-            withPrefix('js/main.js')
-        }
-        /> <
-        script src = {
-            withPrefix('js/misc.js')
-        }
-        /> <
-        script src = {
-            withPrefix('js/page-load.js')
-        }
-        /> <
-        script src = {
-            withPrefix('js/page-unload.js')
-        }
-        /> <
-        script src = {
-            withPrefix('js/prism.js')
-        }
-        /> <
-        script src = {
-            withPrefix('js/search-create.js')
-        }
-        /> <
-        /React.Fragment>
-    ]);
+const React = require( "react" );
+const withPrefix = require( "./src/utils/withPrefix" ).default;
+exports.onRenderBody = function ( {setHeadComponents,setPostBodyComponents} ) {
+    setHeadComponents( [] );
+    setPostBodyComponents( [ 
+    < React.Fragment >
+        < script src={ withPrefix( 'js/addthis.js' ) } />
+        < script src={ withPrefix( 'js/blm.js' ) } />
+        < script src={ withPrefix( 'js/inject.js' ) } />
+        < script src={ withPrefix( 'js/searchbar.js' ) } />
+        < script src={ withPrefix( 'js/plugins.js' ) } />
+        < script src={ withPrefix( 'js/main.js' ) } />
+        < script src={ withPrefix( 'js/misc.js' ) } />
+        < script src={ withPrefix( 'js/page-load.js' ) } />
+        < script src={ withPrefix( 'js/page-unload.js' ) } />
+        < script src={ withPrefix( 'js/prism.js' ) } />
+        < script src={ withPrefix( 'js/search-create.js' ) } />
+    </React.Fragment>
+    ] );
 };
