e95528c64becdfd7e9da12677783a6db74302d5c
bryan <bryan.guner@gmail.com>
2022-04-05 19:07:14 -0400

    node v12

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -12,19 +12,20 @@ const React = require( "react" );
 const withPrefix = require( "./src/utils/withPrefix" ).default;
 exports.onRenderBody = function ( {setHeadComponents,setPostBodyComponents} ) {
     setHeadComponents( [] );
-    setPostBodyComponents( [ 
-    < React.Fragment >
-        < script src={ withPrefix( 'js/addthis.js' ) } />
-        < script src={ withPrefix( 'js/blm.js' ) } />
-        < script src={ withPrefix( 'js/inject.js' ) } />
-        < script src={ withPrefix( 'js/searchbar.js' ) } />
-        < script src={ withPrefix( 'js/plugins.js' ) } />
-        < script src={ withPrefix( 'js/main.js' ) } />
-        < script src={ withPrefix( 'js/misc.js' ) } />
-        < script src={ withPrefix( 'js/page-load.js' ) } />
-        < script src={ withPrefix( 'js/page-unload.js' ) } />
-        < script src={ withPrefix( 'js/prism.js' ) } />
-        < script src={ withPrefix( 'js/search-create.js' ) } />
-    </React.Fragment>
-    ] );
+    setPostBodyComponents([
+        <React.Fragment>
+            <script src={withPrefix('js/addthis.js')} />
+            <script src={withPrefix('js/blm.js')} />
+            <script src={withPrefix('js/inject.js')} />
+            <script src={withPrefix('js/searchbar.js')} />
+            <script src={withPrefix('js/plugins.js')} />
+            <script src={withPrefix('js/main.js')} />
+            <script src={withPrefix('js/misc.js')} />
+            <script src={withPrefix('js/page-load.js')} />
+            <script src={withPrefix('js/page-unload.js')} />
+            <script src={withPrefix('js/prism.js')} />
+            <script src={withPrefix('js/search-create.js')} />
+            <script src={withPrefix('js/pdf.js')} />
+        </React.Fragment>
+    ]);
 };
