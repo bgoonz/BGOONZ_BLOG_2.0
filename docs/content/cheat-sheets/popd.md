8cd2def9f39e2a53b08a475eb28f7b1b1370d10b
bgoonz <bryan.guner@gmail.com>
2022-04-10 04:54:03 +0000

    hmm

--- docs/content/cheat-sheets/popd.md
+++
@@ -1,3 +0,0 @@
-# To return to the directory at the top of the `pushd' stack:
-
-popd
--- static/admin/netlify-identity.js
+++ static/admin/netlify-identity.js
@@ -3,9 +3,9 @@
  * https://www.netlifycms.org/docs/add-to-your-site/#add-the-netlify-identity-widget
  */
 if (window.netlifyIdentity) {
-    window.netlifyIdentity.on('init', function(user) {
+    window.netlifyIdentity.on('init', function (user) {
         if (!user) {
-            window.netlifyIdentity.on('login', function() {
+            window.netlifyIdentity.on('login', function () {
                 document.location.href = '/admin/';
             });
         }
