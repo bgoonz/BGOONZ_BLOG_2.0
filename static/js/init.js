82421bf2e420bf56213bb2b9161bd52b54ece5f0
Restyled.io <commits@restyled.io>
2022-04-19 22:27:35 +0000

    Restyled by prettier

--- static/js/init.js
+++ static/js/init.js
@@ -5,43 +5,54 @@
  * @author Jeff Wainwright <jjwainwright2@gmail.com> (http://jeffry.in)
  * @license MIT
  */
-!(function(e, t) {
+!(function (e, t) {
   "object" == typeof exports && "undefined" != typeof module
-      ? (module.exports = t())
-  : "function" == typeof define && define.amd ? define(t)
-                                              : ((e = e || self).reframe = t());
-})(this, function() {
+    ? (module.exports = t())
+    : "function" == typeof define && define.amd
+    ? define(t)
+    : ((e = e || self).reframe = t());
+})(this, function () {
   "use strict";
-  return function(e, t) {
+  return function (e, t) {
     var i = "string" == typeof e ? document.querySelectorAll(e) : e,
-        n = t || "js-reframe";
-    "length" in i || (i = [ i ]);
+      n = t || "js-reframe";
+    "length" in i || (i = [i]);
     for (var o = 0; o < i.length; o += 1) {
       var r = i[o];
-      if (!(-1 !== r.className.split(" ").indexOf(n) ||
-            -1 < r.style.width.indexOf("%"))) {
-        var d = ((r.getAttribute("height") || r.offsetHeight) /
-                 (r.getAttribute("width") || r.offsetWidth)) *
-                100,
-            f = document.createElement("div");
+      if (
+        !(
+          -1 !== r.className.split(" ").indexOf(n) ||
+          -1 < r.style.width.indexOf("%")
+        )
+      ) {
+        var d =
+            ((r.getAttribute("height") || r.offsetHeight) /
+              (r.getAttribute("width") || r.offsetWidth)) *
+            100,
+          f = document.createElement("div");
         f.className = n;
         var s = f.style;
         (s.position = "relative"), (s.width = "100%"), (s.paddingTop = d + "%");
         var a = r.style;
-        (a.position = "absolute"), (a.width = "100%"), (a.height = "100%"),
-            (a.left = "0"), (a.top = "0"), r.parentNode.insertBefore(f, r),
-            r.parentNode.removeChild(r), f.appendChild(r);
+        (a.position = "absolute"),
+          (a.width = "100%"),
+          (a.height = "100%"),
+          (a.left = "0"),
+          (a.top = "0"),
+          r.parentNode.insertBefore(f, r),
+          r.parentNode.removeChild(r),
+          f.appendChild(r);
       }
     }
   };
 });
 
 // Handle responsive video embeds
-window.addVideoEmbedsHandlers = function() {
+window.addVideoEmbedsHandlers = function () {
   reframe('iframe[src*="youtube.com"],iframe[src*="vimeo.com"]');
 };
 
-window.removeVideoEmbedsHandlers = function() {
+window.removeVideoEmbedsHandlers = function () {
   const frameWrappers = document.querySelectorAll(".js-reframe");
   if (frameWrappers) {
     for (let i = 0; i < frameWrappers.length; i += 1) {
@@ -60,7 +71,7 @@ function navToggleHandler(e) {
   document.body.classList.toggle("js-nav-open");
 }
 
-window.addMainNavigationHandlers = function() {
+window.addMainNavigationHandlers = function () {
   const menuToggle = document.querySelectorAll(".js-nav-toggle");
   if (menuToggle) {
     for (let i = 0; i < menuToggle.length; i++) {
@@ -69,7 +80,7 @@ window.addMainNavigationHandlers = function() {
   }
 };
 
-window.removeMainNavigationHandlers = function() {
+window.removeMainNavigationHandlers = function () {
   document.body.classList.remove("js-nav-open");
   const menuToggle = document.querySelectorAll(".js-nav-toggle");
   if (menuToggle) {
@@ -80,7 +91,7 @@ window.removeMainNavigationHandlers = function() {
 };
 
 // Handle announcement close button click
-const hasLocalStorage = (function() {
+const hasLocalStorage = (function () {
   try {
     localStorage.setItem("__test", true);
     localStorage.removeItem("__test");
@@ -101,7 +112,7 @@ function announcementCloseHandler(e) {
   }
 }
 
-window.addAnnouncementHandlers = function() {
+window.addAnnouncementHandlers = function () {
   const anncmnt = document.querySelector(".js-announcement");
   if (anncmnt) {
     const anncmntClose = document.querySelector(".js-announcment-close");
@@ -116,7 +127,7 @@ window.addAnnouncementHandlers = function() {
   }
 };
 
-window.removeAnnouncementHandlers = function() {
+window.removeAnnouncementHandlers = function () {
   const anncmnt = document.querySelector(".js-announcement");
   if (anncmnt) {
     const anncmntClose = document.querySelector(".js-announcment-close");
@@ -135,8 +146,9 @@ window.removeAnnouncementHandlers = function() {
 function docsNavToggleHandler(e) {
   e.preventDefault();
   // document.body.classList.toggle('docs-menu--opened');
-  document.getElementsByClassName("docs-content")[0].classList.toggle(
-      "docs-menu--opened"); // can't attach to body since we limit the scope of
+  document
+    .getElementsByClassName("docs-content")[0]
+    .classList.toggle("docs-menu--opened"); // can't attach to body since we limit the scope of
   // the docs.scss to the div docs-content (the one
   // just below div with class docs-css), so we add
   // the docs-menu--opened class to it instead of body
@@ -147,7 +159,7 @@ function submenuToggleHandler(e) {
   this.parentNode.classList.toggle("active");
 }
 
-window.addDocsNavigationHandlers = function() {
+window.addDocsNavigationHandlers = function () {
   const docsNavToggle = document.getElementById("docs-nav-toggle");
   if (docsNavToggle) {
     docsNavToggle.addEventListener("click", docsNavToggleHandler, false);
@@ -156,13 +168,16 @@ window.addDocsNavigationHandlers = function() {
   const docsSubmenuToggle = document.querySelectorAll(".docs-submenu-toggle");
   if (docsSubmenuToggle) {
     for (let i = 0; i < docsSubmenuToggle.length; i++) {
-      docsSubmenuToggle[i].addEventListener("click", submenuToggleHandler,
-                                            false);
+      docsSubmenuToggle[i].addEventListener(
+        "click",
+        submenuToggleHandler,
+        false
+      );
     }
   }
 };
 
-window.removeDocsNavigationHandlers = function() {
+window.removeDocsNavigationHandlers = function () {
   // Remove docs nav related classes on page load
   document.body.classList.remove("docs-menu--opened");
 
@@ -174,14 +189,17 @@ window.removeDocsNavigationHandlers = function() {
   const docsSubmenuToggle = document.querySelectorAll(".docs-submenu-toggle");
   if (docsSubmenuToggle) {
     for (let i = 0; i < docsSubmenuToggle.length; i++) {
-      docsSubmenuToggle[i].removeEventListener("click", submenuToggleHandler,
-                                               false);
+      docsSubmenuToggle[i].removeEventListener(
+        "click",
+        submenuToggleHandler,
+        false
+      );
     }
   }
 };
 
 // Table of contents for the docs section
-window.addPageNavLinks = function() {
+window.addPageNavLinks = function () {
   const pageToc = document.getElementById("page-nav-inside");
   const pageTocContainer = document.getElementById("page-nav-link-container");
 
@@ -190,7 +208,7 @@ window.addPageNavLinks = function() {
 
     // Create in-page navigation
     const headerLinks = getHeaderLinks({
-      root : pageContent,
+      root: pageContent,
     });
     if (headerLinks.length > 0) {
       pageToc.classList.add("has-links");
@@ -201,7 +219,7 @@ window.addPageNavLinks = function() {
     let scroll = new SmoothScroll("[data-scroll]");
     let hash = window.decodeURI(location.hash.replace("#", ""));
     if (hash !== "") {
-      window.setTimeout(function() {
+      window.setTimeout(function () {
         let anchor = document.getElementById(hash);
         if (anchor) {
           scroll.animateScroll(anchor);
@@ -213,13 +231,13 @@ window.addPageNavLinks = function() {
     let pageTocLinks = pageTocContainer.getElementsByTagName("a");
     if (pageTocLinks.length > 0) {
       let spy = new Gumshoe("#page-nav-inside a", {
-        nested : true,
-        nestedClass : "active-parent",
+        nested: true,
+        nestedClass: "active-parent",
       });
     }
 
     // Add link to page content headings
-    let pageHeadings = getElementsByTagNames(pageContent, [ "h2", "h3" ]);
+    let pageHeadings = getElementsByTagNames(pageContent, ["h2", "h3"]);
     for (let i = 0; i < pageHeadings.length; i++) {
       let heading = pageHeadings[i];
       if (typeof heading.id !== "undefined" && heading.id !== "") {
@@ -229,15 +247,17 @@ window.addPageNavLinks = function() {
 
     // Copy link url
     let clipboard = new ClipboardJS(".hash-link", {
-      text : function(trigger) {
-        return (window.location.href.replace(window.location.hash, "") +
-                trigger.getAttribute("href"));
+      text: function (trigger) {
+        return (
+          window.location.href.replace(window.location.hash, "") +
+          trigger.getAttribute("href")
+        );
       },
     });
   }
 };
 
-window.removePageNavLinks = function() {
+window.removePageNavLinks = function () {
   const pageToc = document.getElementById("page-nav-inside");
   const pageTocContainer = document.getElementById("page-nav-link-container");
 
@@ -266,8 +286,8 @@ function createLinksForHeaderElements(elements) {
   let result = [];
   let stack = [
     {
-      level : 0,
-      children : result,
+      level: 0,
+      children: result,
     },
   ];
   let re = /^h(\d)$/;
@@ -283,25 +303,27 @@ function createLinksForHeaderElements(elements) {
     if (!element.id) {
       if (!element.textContent) {
         console.warn(
-            "can not create link to element without id and without text content");
+          "can not create link to element without id and without text content"
+        );
         continue;
       }
-      element.id = element.textContent.toLowerCase()
-                       .replace(/[^\w]+/g, "_")
-                       .replace(/^_/, "")
-                       .replace(/_$/, "");
+      element.id = element.textContent
+        .toLowerCase()
+        .replace(/[^\w]+/g, "_")
+        .replace(/^_/, "")
+        .replace(/_$/, "");
     }
     let link = document.createElement("a");
     link.href = "#" + element.id;
     link.setAttribute("data-scroll", "");
     link.appendChild(document.createTextNode(element.textContent));
     let obj = {
-      id : element.id,
-      level : headerLevel,
-      textContent : element.textContent,
-      element : element,
-      link : link,
-      children : [],
+      id: element.id,
+      level: headerLevel,
+      textContent: element.textContent,
+      element: element,
+      link: link,
+      children: [],
     };
     if (headerLevel > stack[stack.length - 1].level) {
       stack[stack.length - 1].children.push(obj);
@@ -318,7 +340,7 @@ function createLinksForHeaderElements(elements) {
 }
 
 function getHeaderLinks(options = {}) {
-  let tagNames = options.tagNames || [ "h2", "h3" ];
+  let tagNames = options.tagNames || ["h2", "h3"];
   let root = options.root || document.body;
   let headerElements = getElementsByTagNames(root, tagNames);
   return createLinksForHeaderElements(headerElements);
@@ -346,7 +368,7 @@ function anchorForId(id) {
   anchor.setAttribute("data-scroll", "");
   anchor.href = "#" + id;
   anchor.innerHTML =
-      '<span class="icon-copy" aria-hidden="true"></span><span class="screen-reader-text">Copy</span>';
+    '<span class="icon-copy" aria-hidden="true"></span><span class="screen-reader-text">Copy</span>';
   return anchor;
 }
 
