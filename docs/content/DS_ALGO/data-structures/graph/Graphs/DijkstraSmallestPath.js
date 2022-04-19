e56d74831f7dc7c5ff19ac78d78b80cf13fa92af
Restyled.io <commits@restyled.io>
2022-04-19 06:12:15 +0000

    Restyled by clang-format

--- docs/content/DS_ALGO/data-structures/graph/Graphs/DijkstraSmallestPath.js
+++ docs/content/DS_ALGO/data-structures/graph/Graphs/DijkstraSmallestPath.js
@@ -1,43 +1,43 @@
 // starting at s
 function solve(graph, s) {
-    const solutions = {};
-    solutions[s] = [];
-    solutions[s].dist = 0;
-    while (true) {
-        let p = null;
-        let neighbor = null;
-        let dist = Infinity;
-        for (const n in solutions) {
-            if (!solutions[n]) {
-                continue;
-            }
-            const ndist = solutions[n].dist;
-            const adj = graph[n];
-            for (const a in adj) {
-                if (solutions[a]) {
-                    continue;
-                }
-                const d = adj[a] + ndist;
-                if (d < dist) {
-                    p = solutions[n];
-                    neighbor = a;
-                    dist = d;
-                }
-            }
+  const solutions = {};
+  solutions[s] = [];
+  solutions[s].dist = 0;
+  while (true) {
+    let p = null;
+    let neighbor = null;
+    let dist = Infinity;
+    for (const n in solutions) {
+      if (!solutions[n]) {
+        continue;
+      }
+      const ndist = solutions[n].dist;
+      const adj = graph[n];
+      for (const a in adj) {
+        if (solutions[a]) {
+          continue;
         }
-        // no more solutions
-        if (dist === Infinity) {
-            break;
+        const d = adj[a] + ndist;
+        if (d < dist) {
+          p = solutions[n];
+          neighbor = a;
+          dist = d;
         }
-        // extend parent's solution path
-        solutions[neighbor] = p.concat(neighbor);
-        // extend parent's cost
-        solutions[neighbor].dist = dist;
+      }
     }
-    return solutions;
+    // no more solutions
+    if (dist === Infinity) {
+      break;
+    }
+    // extend parent's solution path
+    solutions[neighbor] = p.concat(neighbor);
+    // extend parent's cost
+    solutions[neighbor].dist = dist;
+  }
+  return solutions;
 }
 
-export { solve };
+export {solve};
 
 // // create graph
 // const graph = {}
@@ -82,8 +82,8 @@ export { solve };
 // // get all solutions
 // const solutions = solve(graph, start)
 // // for s in solutions..
-// ' -> ' + s + ': [' + solutions[s].join(', ') + ']   (dist:' + solutions[s].dist + ')'
-// From '10' to
+// ' -> ' + s + ': [' + solutions[s].join(', ') + ']   (dist:' +
+// solutions[s].dist + ')' From '10' to
 //  -> 2: [7, 5, 4, 2]   (dist:4)
 //  -> 3: [7, 5, 4, 3]   (dist:4)
 //  -> 4: [7, 5, 4]   (dist:3)
--- docs/content/DS_ALGO/data-structures/graph/Graphs/NumberOfIslands.js
+++ docs/content/DS_ALGO/data-structures/graph/Graphs/NumberOfIslands.js
@@ -1,19 +1,11 @@
 /* Number of Islands
 https://dev.to/rattanakchea/amazons-interview-question-count-island-21h6
-Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
-a two dimensional grid map
-each element is going to represent a piece of land
-1 is land,
-0 is water
-output a number which is the number of islands
-Example 1:
-  Input:
-  11110
-  11010
-  11000
-  00000
-  Output: 1
-Example 2:
+Given a 2d grid map of '1's (land) and '0's (water), count the number of
+islands. An island is surrounded by water and is formed by connecting adjacent
+lands horizontally or vertically. You may assume all four edges of the grid are
+all surrounded by water. a two dimensional grid map each element is going to
+represent a piece of land 1 is land, 0 is water output a number which is the
+number of islands Example 1: Input: 11110 11010 11000 00000 Output: 1 Example 2:
   Input:
   11000
   11000
@@ -37,32 +29,35 @@ Pseudocode:
     Return the count
 */
 const islands = (matrixGrid) => {
-    const matrix = matrixGrid;
-    let counter = 0;
-    const flood = (row, col) => {
-        if (row < 0 || col < 0) return; // Off the map above or left
-        if (row >= matrix.length || col >= matrix[row].length) return; // Off the map below or right
-        const tile = matrix[row][col];
-        if (tile !== '1') return;
-        matrix[row][col] = '0';
-        flood(row + 1, col); // Down
-        flood(row - 1, col); // Up
-        flood(row, col + 1); // Right
-        flood(row, col - 1); // Left
-    };
-    for (let row = 0; row < matrix.length; row += 1) {
-        for (let col = 0; col < matrix[row].length; col += 1) {
-            const current = matrix[row][col];
-            if (current === '1') {
-                flood(row, col);
-                counter += 1;
-            }
-        }
+  const matrix = matrixGrid;
+  let counter = 0;
+  const flood = (row, col) => {
+    if (row < 0 || col < 0)
+      return; // Off the map above or left
+    if (row >= matrix.length || col >= matrix[row].length)
+      return; // Off the map below or right
+    const tile = matrix[row][col];
+    if (tile !== '1')
+      return;
+    matrix[row][col] = '0';
+    flood(row + 1, col); // Down
+    flood(row - 1, col); // Up
+    flood(row, col + 1); // Right
+    flood(row, col - 1); // Left
+  };
+  for (let row = 0; row < matrix.length; row += 1) {
+    for (let col = 0; col < matrix[row].length; col += 1) {
+      const current = matrix[row][col];
+      if (current === '1') {
+        flood(row, col);
+        counter += 1;
+      }
     }
-    return counter;
+  }
+  return counter;
 };
 
-export { islands };
+export {islands};
 
 // islands(
 //   ['1', '1', '0', '0', '0'],
