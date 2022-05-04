ea9295c2f252958e76236c5c74588c74b4327708
Restyled.io <commits@restyled.io>
2022-05-04 07:51:04 +0000

    Restyled by prettier

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,26 +12,36 @@ let initialized = false;
 
 exports.onInitialClientRender = () => {
   initialized = true;
-  if ('onGatsbyInitialClientRender' in window &&
-      typeof window.onGatsbyInitialClientRender === 'function') {
+  if (
+    "onGatsbyInitialClientRender" in window &&
+    typeof window.onGatsbyInitialClientRender === "function"
+  ) {
     window.onGatsbyInitialClientRender();
   }
-  if ('onGatsbyRouteUpdate' in window &&
-      typeof window.onGatsbyRouteUpdate === 'function') {
+  if (
+    "onGatsbyRouteUpdate" in window &&
+    typeof window.onGatsbyRouteUpdate === "function"
+  ) {
     window.onGatsbyRouteUpdate();
   }
 };
 
 exports.onRouteUpdate = () => {
-  if (initialized && 'onGatsbyRouteUpdate' in window &&
-      typeof window.onGatsbyRouteUpdate === 'function') {
+  if (
+    initialized &&
+    "onGatsbyRouteUpdate" in window &&
+    typeof window.onGatsbyRouteUpdate === "function"
+  ) {
     window.onGatsbyRouteUpdate();
   }
 };
 
 exports.onPreRouteUpdate = () => {
-  if (initialized && 'onGatsbyPreRouteUpdate' in window &&
-      typeof window.onGatsbyPreRouteUpdate === 'function') {
+  if (
+    initialized &&
+    "onGatsbyPreRouteUpdate" in window &&
+    typeof window.onGatsbyPreRouteUpdate === "function"
+  ) {
     window.onGatsbyPreRouteUpdate();
   }
 };
