6b478608bcaee7d107c822e1089fa0d4b77a28f8
Restyled.io <commits@restyled.io>
2022-05-04 07:51:01 +0000

    Restyled by clang-format

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,30 +4,34 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// onPreRouteUpdate() and onRouteUpdate() are called before onInitialClientRender,
-// use initialized flag to ensure that window.onGatsbyPreRouteUpdate() and
-// window.onGatsbyRouteUpdate() will not be called before
-// window.onGatsbyInitialClientRender() has run
+// onPreRouteUpdate() and onRouteUpdate() are called before
+// onInitialClientRender, use initialized flag to ensure that
+// window.onGatsbyPreRouteUpdate() and window.onGatsbyRouteUpdate() will not be
+// called before window.onGatsbyInitialClientRender() has run
 let initialized = false;
 
 exports.onInitialClientRender = () => {
-    initialized = true;
-    if ('onGatsbyInitialClientRender' in window && typeof window.onGatsbyInitialClientRender === 'function') {
-        window.onGatsbyInitialClientRender();
-    }
-    if ('onGatsbyRouteUpdate' in window && typeof window.onGatsbyRouteUpdate === 'function') {
-        window.onGatsbyRouteUpdate();
-    }
+  initialized = true;
+  if ('onGatsbyInitialClientRender' in window &&
+      typeof window.onGatsbyInitialClientRender === 'function') {
+    window.onGatsbyInitialClientRender();
+  }
+  if ('onGatsbyRouteUpdate' in window &&
+      typeof window.onGatsbyRouteUpdate === 'function') {
+    window.onGatsbyRouteUpdate();
+  }
 };
 
 exports.onRouteUpdate = () => {
-    if (initialized && 'onGatsbyRouteUpdate' in window && typeof window.onGatsbyRouteUpdate === 'function') {
-        window.onGatsbyRouteUpdate();
-    }
+  if (initialized && 'onGatsbyRouteUpdate' in window &&
+      typeof window.onGatsbyRouteUpdate === 'function') {
+    window.onGatsbyRouteUpdate();
+  }
 };
 
 exports.onPreRouteUpdate = () => {
-    if (initialized && 'onGatsbyPreRouteUpdate' in window && typeof window.onGatsbyPreRouteUpdate === 'function') {
-        window.onGatsbyPreRouteUpdate();
-    }
+  if (initialized && 'onGatsbyPreRouteUpdate' in window &&
+      typeof window.onGatsbyPreRouteUpdate === 'function') {
+    window.onGatsbyPreRouteUpdate();
+  }
 };
