939cc567b31cbc39dadc156f3f059d4a507f3e68
bgoonz <bryan.guner@gmail.com>
2022-05-04 17:19:29 +0000

    identity

--- static/admin/netlify-identity.js
+++ static/admin/netlify-identity.js
@@ -1,29 +1,13 @@
-<<<<<<< HEAD
 /**
  * This script is taken from:
  * https://www.netlifycms.org/docs/add-to-your-site/#add-the-netlify-identity-widget
  */
 if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", function (user) {
-    if (!user) {
-      window.netlifyIdentity.on("login", function () {
-        document.location.href = "/admin/";
-      });
-    }
-  });
+    window.netlifyIdentity.on("init", function(user) {
+        if (!user) {
+            window.netlifyIdentity.on("login", function() {
+                document.location.href = "/admin/";
+            });
+        }
+    });
 }
-=======
-/**
- * This script is taken from:
- * https://www.netlifycms.org/docs/add-to-your-site/#add-the-netlify-identity-widget
- */
-if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", function (user) {
-    if (!user) {
-      window.netlifyIdentity.on("login", function () {
-        document.location.href = "/admin/";
-      });
-    }
-  });
-}
->>>>>>> eb4e776211cb70e9dd66aac61069ac7a52f1de84
