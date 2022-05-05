4d1d4466e0bd51e4c2760e136ded377da5685301
Bryan C Guner <bryan.guner@gmail.com>
2022-05-05 16:36:11 +0000

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,45 @@
-const siteMetadata = require( './site-metadata.json' )
+const siteMetadata = require('./site-metadata.json')
 module.exports = {
-  pathPrefix: '/',
-  siteMetadata: siteMetadata,
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-source-data`,
-    `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages`
-      }
-    },
-    {
-      resolve: `gatsby-plugin-disqus`,
-      options: {
-        shortname: `webdevhub-1`,
-      }
-    },
-    {
-      resolve: `gatsby-plugin-sass`,
-      options: {}
-    },
-    {
-      resolve: `gatsby-remark-page-creator`,
-      options: {}
-    },
-    {
-      resolve: `@stackbit/gatsby-plugin-menus`,
-      options: {
-        sourceUrlPath: `fields.url`,
-        pageContextProperty: `menus`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-disqus`,
-      options: {
-        shortname: `webdevhub-1`
-      }
-    },
-  ]
+    pathPrefix: '/',
+    siteMetadata: siteMetadata,
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-source-data`,
+        `gatsby-transformer-remark`,
+        `gatsby-plugin-netlify`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `pages`,
+                path: `${__dirname}/src/pages`
+            }
+        },
+        {
+            resolve: `gatsby-plugin-disqus`,
+            options: {
+                shortname: `webdevhub-1`,
+            }
+        },
+        {
+            resolve: `gatsby-plugin-sass`,
+            options: {}
+        },
+        {
+            resolve: `gatsby-remark-page-creator`,
+            options: {}
+        },
+        {
+            resolve: `@stackbit/gatsby-plugin-menus`,
+            options: {
+                sourceUrlPath: `fields.url`,
+                pageContextProperty: `menus`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-disqus`,
+            options: {
+                shortname: `webdevhub-1`
+            }
+        },
+    ]
 };
