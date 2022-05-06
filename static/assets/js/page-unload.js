e1ac05c49841b2533915847d0782571edd3a65d4
Bryan C Guner <bryan.guner@gmail.com>
2022-05-06 02:56:53 +0000

    update

--- static/assets/js/page-unload.js
+++ static/assets/js/page-unload.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.onGatsbyPreRouteUpdate = function () {
   window.removeMainNavigationHandlers();
   window.removeAnnouncementHandlers();
@@ -6,12 +5,3 @@ window.onGatsbyPreRouteUpdate = function () {
   window.removeDocsNavigationHandlers();
   window.removePageNavLinks();
 };
-=======
-window.onGatsbyPreRouteUpdate = function () {
-  window.removeMainNavigationHandlers();
-  window.removeAnnouncementHandlers();
-  window.removeVideoEmbedsHandlers();
-  window.removeDocsNavigationHandlers();
-  window.removePageNavLinks();
-};
->>>>>>> eb4e776211cb70e9dd66aac61069ac7a52f1de84
