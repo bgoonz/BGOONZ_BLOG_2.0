e4770c6edaebed7249e2a1a3350c1bb012598b26
Bryan C Guner <bryan.guner@gmail.com>
2022-05-16 10:47:56 +0000

    hi

--- netlify/.bin/gl-style-validate.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\@mapbox\mapbox-gl-style-spec\bin\gl-style-validate.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\@mapbox\mapbox-gl-style-spec\bin\gl-style-validate.js" %*
-)
--- netlify/.bin/st.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\st\bin\server.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\st\bin\server.js" %*
-)
