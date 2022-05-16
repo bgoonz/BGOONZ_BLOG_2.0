7970fce762e589000090b6845b817fa9e35f08fe
Bryan C Guner <bryan.guner@gmail.com>
2022-05-16 08:27:55 +0000

    update

---
+++ scripts/es6/components/index.js
@@ -0,0 +1,46 @@
+import ActionLink from './ActionLink';
+import CtaButtons from './CtaButtons';
+import DocsMenu from './DocsMenu';
+import DocsSubmenu from './DocsSubmenu';
+import Footer from './Footer';
+import Header from './Header';
+import Icon from './Icon';
+import Layout from './Layout';
+import SectionContent from './SectionContent';
+import SectionCta from './SectionCta';
+import SectionDocs from './SectionDocs';
+import SectionGrid from './SectionGrid';
+import SectionHero from './SectionHero';
+import Submenu from './Submenu';
+export {
+    ActionLink,
+    CtaButtons,
+    DocsMenu,
+    DocsSubmenu,
+    Footer,
+    Header,
+    Icon,
+    SectionContent,
+    SectionCta,
+    SectionDocs,
+    SectionGrid,
+    SectionHero,
+    Submenu,
+    Layout
+};
+export default {
+    ActionLink,
+    CtaButtons,
+    DocsMenu,
+    DocsSubmenu,
+    Footer,
+    Header,
+    Icon,
+    SectionContent,
+    SectionCta,
+    SectionDocs,
+    SectionGrid,
+    SectionHero,
+    Submenu,
+    Layout
+};
