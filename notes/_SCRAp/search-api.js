ace62ce5b0c5fe76fa8d8cacbf0ccbd66da3ee6c
Bryan C Guner <bryan.guner@gmail.com>
2022-05-16 00:58:37 +0000

    update

--- notes/_SCRAp/search-api.js
+++ notes/_SCRAp/search-api.js
@@ -5,16 +5,8 @@ let config = {
     url: '{{host}}/v1/account',
     headers: {},
     timeout: 100
-}; <<
-<< << < HEAD
-axios(config)
-    .then(function(response) {
-        console.log(JSON.stringify(response.data));
-    })
-    .catch(function(error) {
-        console.log(error);
-    }); ===
-=== =
+};
+
 
 axios(config)
     .then(function(response) {
@@ -22,5 +14,4 @@ axios(config)
     })
     .catch(function(error) {
         console.log(error);
-    }); >>>
->>> > 1 f628061e(formatting)
+    });
