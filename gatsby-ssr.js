dec1a662a43f0cecb442894639da87eff002505d
bryan <bryan.guner@gmail.com>
2022-05-16 06:32:00 -0400

    messed w stuff

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -13,20 +13,21 @@ const withPrefix = require("./src/utils/withPrefix").default;
 exports.onRenderBody = function ({ setHeadComponents, setPostBodyComponents }) {
     setHeadComponents([]);
     setPostBodyComponents([
-        < React.Fragment >
-            < script src={withPrefix('js/addthis.js')} />
-            < script src={withPrefix('js/blm.js')} />
-            < script src={withPrefix('js/inject.js')} />
-            < script src={withPrefix('js/searchbar.js')} />
-            < script src={withPrefix('js/plugins.js')} />
-            < script src={withPrefix('js/main.js')} />
-            < script src={withPrefix('js/misc.js')} />
-            < script src={withPrefix('js/page-load.js')} />
-            < script src={withPrefix('js/page-unload.js')} />
-            < script src={withPrefix('js/prism.js')} />
-            < script src={withPrefix('js/search-create.js')} />
+        <React.Fragment>
+            <script src={withPrefix('js/addthis.js')} />
+            <script src={withPrefix('js/blm.js')} />
+            <script src={withPrefix('js/inject.js')} />
+            <script src={withPrefix('js/searchbar.js')} />
+            <script src={withPrefix('js/plugins.js')} />
+            <script src={withPrefix('js/main.js')} />
+            <script src={withPrefix('js/misc.js')} />
+            <script src={withPrefix('js/page-load.js')} />
+            <script src={withPrefix('js/page-unload.js')} />
+            <script src={withPrefix('js/prism.js')} />
+            <script src={withPrefix('js/search-create.js')} />
             <script src={withPrefix('js/algolia.js')} />
- <script src={withPrefix('js/syntax.js')} />
+            <script src={('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.5/highlight.min.js')} />
+            <script src={withPrefix('js/syntax.js')} />
         </React.Fragment>
     ]);
 };
