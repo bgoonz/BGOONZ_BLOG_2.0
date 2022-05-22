266eb18464bc3bf9550a2bba69ad330a4b987ba3
Bryan C Guner <66654881+bgoonz@users.noreply.github.com>
2022-05-22 03:04:02 -0400

    footer and static scripts

--- static/js/print-2-pdf.js
+++ static/js/print-2-pdf.js
@@ -12,7 +12,6 @@ api.convertUrlToFile("currentUrl", "document.pdf", function(err, fileName) {
 
 function injectPDF (){
       let anchor = window.createElement("a");
-      anchor.onclick=`${if(!this.p)href+='&url='+encodeURIComponent(location.href);this.p=1}`
+      anchor.onclick="if(!this.p)href+='&url='+encodeURIComponent(location.href);this.p=1"
       anchor.textContent='Save to PDF'
 }
-<a href="//pdfcrowd.com/url_to_pdf/?" onclick="if(!this.p)href+='&url='+encodeURIComponent(location.href);this.p=1">Save to PDF</a>
