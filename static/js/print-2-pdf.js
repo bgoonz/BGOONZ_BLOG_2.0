abd618a51d794ca1425d57b16e9406acff0afb23
Restyled.io <commits@restyled.io>
2022-05-23 09:51:43 +0000

    Restyled by clang-format

--- static/js/print-2-pdf.js
+++ static/js/print-2-pdf.js
@@ -1,13 +1,14 @@
 // install: "npm install pdfcrowd"
 let currentUrl = window.location.href;
-import { HtmlToPdfClient } from 'pdfcrowd';
+import {HtmlToPdfClient} from 'pdfcrowd';
 let api = new HtmlToPdfClient('demo', 'ce544b6ea52a5621fb9d55f8b542d14d');
-api.convertUrlToFile('currentUrl', 'document.pdf', function (err, fileName) {
-    /* done */
+api.convertUrlToFile('currentUrl', 'document.pdf', function(err, fileName) {
+  /* done */
 });
 
 const onclick = () => {
-    let anchor = window.createElement('a');
-    anchor.onclick = "if(!this.p)href+='&url='+encodeURIComponent(location.href);this.p=1";
-    anchor.textContent = 'Save to PDF';
+  let anchor = window.createElement('a');
+  anchor.onclick =
+      "if(!this.p)href+='&url='+encodeURIComponent(location.href);this.p=1";
+  anchor.textContent = 'Save to PDF';
 };
