8379996fe59c581f83fca70f32992511e5fe8751
Bryan-Guner-Backup <bgoonz4212@gmail.com>
2022-05-23 08:56:32 +0000

    hi"

--- public/js/searchbar.js
+++ public/js/searchbar.js
@@ -3,8 +3,8 @@
   var t =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZTQzZmFlY2I2NjkwNWMyZGJkMGVmZCIsInRva2VuVHlwZSI6InB1YmxpYyIsImlhdCI6MTY0MjM0ODUyNiwiZXhwIjoxNDI1Njc0ODUyNn0.VD7Lxgh5cWuOzOhV9KH51TZVdPVXvsIJgRUx-oacfVM";
   function e() {
-    var e = document.createElement("script"),
-      a = navigator.language || navigator.userLanguage;
+    var e = document.createElement("script");
+    var a = navigator.language || navigator.userLanguage;
     (e.type = "text/javascript"),
       (e.async = !0),
       (e.src = "https://api.searchbar.org/v1/widget/" + t + "/" + a);
