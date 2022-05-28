6a9967e0542e57d7da89226e1b11c125220b2a5b
Bryan-Guner-Backup <bgoonz4212@gmail.com>
2022-05-28 22:02:13 +0000

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,12 @@ module.exports = {
             }
         },
         {
+            resolve: 'gatsby-transformer-remark',
+            options: {
+              plugins: ['gatsby-remark-code-buttons']
+            }
+          },
+        {
             resolve: `gatsby-plugin-sass`,
             options: {}
         },
