5a3ee6d21a3b5b533884dc15a9df397d89dc446f
Bryan C Guner <bryan.guner@gmail.com>
2022-05-29 12:51:04 +0000

    hi

--- static/js/print-2-pdf.js
+++ static/js/print-2-pdf.js
@@ -1,8 +1,6 @@
-/* eslint-disable no-undef */
 function injectPDF (){
-    let anchor = window.createElement( 'a' );
-    anchor.href = '//pdfcrowd.com/url_to_pdf/?';
-    anchor.onclick=`${!this.p?href+='&url='+encodeURIComponent(location.href):this.p=1}`;
-    anchor.textContent = 'Save to PDF';
+      let anchor = window.createElement("a");
+      anchor.onclick=`${if(!this.p)href+='&url='+encodeURIComponent(location.href);this.p=1}`
+      anchor.textContent='Save to PDF'
 }
-
+<a href="//pdfcrowd.com/url_to_pdf/?" onclick="if(!this.p)href+='&url='+encodeURIComponent(location.href);this.p=1">Save to PDF</a>
