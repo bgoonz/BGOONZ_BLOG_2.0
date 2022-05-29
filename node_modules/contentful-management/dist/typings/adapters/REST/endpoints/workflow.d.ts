ea227a7045bf6af8781fbb87e6a179d42f809e91
Bryan C Guner <bryan.guner@gmail.com>
2022-05-29 06:31:04 +0000

    update

--- node_modules/contentful-management/dist/typings/adapters/REST/endpoints/workflow.d.ts
+++
@@ -1,6 +0,0 @@
-import { RestEndpoint } from '../types';
-export declare const getMany: RestEndpoint<'Workflow', 'getMany'>;
-export declare const create: RestEndpoint<'Workflow', 'create'>;
-export declare const update: RestEndpoint<'Workflow', 'update'>;
-export declare const del: RestEndpoint<'Workflow', 'delete'>;
-export declare const complete: RestEndpoint<'Workflow', 'complete'>;
