11814353a10fe03d926ecc1759a31184b9f4e3b8
Bryan C Guner <bryan.guner@gmail.com>
2022-06-12 06:25:10 -0400

    components

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -1,16 +1,16 @@
+/* This class is a React component that renders a section of the page. It has a title, content, and a
+button. */
 import React from 'react';
 import _ from 'lodash';
-
 import {toStyleObj, withPrefix, markdownify} from '../utils';
 import CtaButtons from './CtaButtons';
-
 export default class SectionHero extends React.Component {
     render() {
         let section = _.get(this.props, 'section', null);
         return (
             <section id={_.get(section, 'section_id', null)} className="block block-hero has-gradient outer">
               {_.get(section, 'image', null) && (
-              <div className="bg-img" style={toStyleObj('background-image: url(\'' + withPrefix(_.get(section, 'image', null)) + '\')')}/>
+              <div className="bg-img" style={toStyleObj(`background-image: url('${withPrefix(_.get(section, 'image', null))}')`)}/>
               )}
               <div className="inner-sm">
                 {_.get(section, 'title', null) && (
