d8a871171a7fdc36d97a9db4d1e0f5fa98fe732b
Bryan C Guner <bryan.guner@gmail.com>
2022-06-15 11:57:48 -0400

    Add files via upload

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,18 +10,6 @@
 // window.onGatsbyRouteUpdate() will not be called before
 // window.onGatsbyInitialClientRender() has run
 let initialized = false;
-const React = require("react")
-const Layout = require("./src/components/layout")
-// Logs when the client route changes
-exports.onRouteUpdate = ({location, prevLocation}) => {
-    console.log("new pathname", location.pathname)
-    console.log("old pathname", prevLocation ? prevLocation.pathname : null)
-}
-// Wraps every page in a component
-exports.wrapPageElement = ({element, props}) => {
-    <Layout {...props}>
-        {element} </Layout>
-}
 
 exports.onInitialClientRender = () => {
     initialized = true;
