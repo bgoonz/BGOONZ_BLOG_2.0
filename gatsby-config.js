2d894a59d9ed96eee4d72f80e3c6cd96059f4e29
Bryan C Guner <bryan.guner@gmail.com>
2022-06-18 02:42:23 +0000

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     plugins: [
         'gatsby-plugin-react-helmet',
         'gatsby-source-data',
+
         {
             resolve: 'gatsby-source-filesystem',
             options: {
@@ -15,7 +16,7 @@ module.exports = {
         {
             resolve: 'gatsby-transformer-remark',
             options: {
-                plugins: ['gatsby-remark-prismjs-copy-button', 'gatsby-remark-prismjs'],
+                plugins: ['gatsby-remark-prismjs'],
             },
         },
         {
