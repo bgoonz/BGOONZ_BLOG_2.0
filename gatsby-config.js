e7b81cfdd090f5c8090a68fdc305e6f90d13edb8
Bryan C Guner <bryan.guner@gmail.com>
2022-06-21 04:05:01 +0000

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,49 +1,43 @@
-const siteMetadata = require('./site-metadata.json');
+const siteMetadata = require('./site-metadata.json')
 module.exports = {
     pathPrefix: '/',
     siteMetadata: siteMetadata,
     plugins: [
-        'gatsby-plugin-react-helmet',
-        'gatsby-source-data',
+        `gatsby-plugin-react-helmet`,
+        `gatsby-source-data`,
 
         {
-            resolve: 'gatsby-source-filesystem',
+            resolve: `gatsby-source-filesystem`,
             options: {
-                name: 'pages',
+                name: `pages`,
                 path: `${__dirname}/src/pages`
             }
         },
         {
-            resolve: 'gatsby-transformer-remark',
+            resolve: `gatsby-plugin-disqus`,
             options: {
-                plugins: ['gatsby-remark-prismjs'],
-            },
-        },
-        {
-            resolve: 'gatsby-plugin-disqus',
-            options: {
-                shortname: 'webdevhub-1',
+                shortname: `webdevhub-1`,
             }
         },
         {
-            resolve: 'gatsby-plugin-sass',
+            resolve: `gatsby-plugin-sass`,
             options: {}
         },
         {
-            resolve: 'gatsby-remark-page-creator',
+            resolve: `gatsby-remark-page-creator`,
             options: {}
         },
         {
-            resolve: '@stackbit/gatsby-plugin-menus',
+            resolve: `@stackbit/gatsby-plugin-menus`,
             options: {
-                sourceUrlPath: 'fields.url',
-                pageContextProperty: 'menus',
+                sourceUrlPath: `fields.url`,
+                pageContextProperty: `menus`,
             },
         },
         {
-            resolve: 'gatsby-plugin-disqus',
+            resolve: `gatsby-plugin-disqus`,
             options: {
-                shortname: 'webdevhub-1'
+                shortname: `webdevhub-1`
             }
         },
     ]
