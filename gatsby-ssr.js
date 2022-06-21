7ad342c47eba1c6acb0963fb0b048fd677c3491f
Bryan C Guner <bryan.guner@gmail.com>
2022-06-21 13:18:31 +0000

    master

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -15,12 +15,7 @@ exports.onRenderBody = function ({ setHeadComponents, setPostBodyComponents }) {
     setPostBodyComponents([
         <React.Fragment>
             <script src={withPrefix('js/js/addthis.js')} />
-            <script src={withPrefix('js/algolia.js')}/>
-            <script src={withPrefix('js/analytics.js')}/>
-            <script src={withPrefix('js/blm.js')}/>
             <script src={withPrefix('js/docs-main.js')}/>
-            <script src={withPrefix('js/google-search.js')}/>
-            <script src={withPrefix('js/goog-search.js')}/>
             <script src={withPrefix('js/init.js')}/>
             <script src={withPrefix('js/inject.js')}/>
             <script src={withPrefix('js/jsfiddle-integration.js')}/>
@@ -39,9 +34,7 @@ exports.onRenderBody = function ({ setHeadComponents, setPostBodyComponents }) {
             <script src={withPrefix('js/toPlainText.js')}/>
             <script src={withPrefix('js/ukraine.js')}/>
             <script src={withPrefix('js/syntax.js')} />
-            <script src = {'https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js'} />
             <script src={'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.5/highlight.min.js'} />
-            <script src={'https://www.googletagmanager.com/gtag/js?id=G-90VG64DEP3'} />
         </React.Fragment>
     ]);
 };
