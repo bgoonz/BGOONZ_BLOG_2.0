eccbd529820ce0a057b37d37fbc766e47c1b740b
impastagui <bgoonz@vivaldi.net>
2022-06-28 20:29:41 +0000

    commit

--- static/js/blm.js
+++ static/js/blm.js
@@ -168,6 +168,7 @@ BLMBadge.init({
     title: '#BlackLivesMatter',
     imageAlt: 'Black Lives Matter Badge'
 });
+
 function MakerBadge(t) {
     (this.userId = ''),
         (this.position = 'right'),
