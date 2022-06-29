79e4770bccfe1f16f7b903dc5ee616cdd0b1ebad
Bryan C Guner <bryan.guner@gmail.com>
2022-06-29 07:53:44 +0000

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
         'gatsby-plugin-react-helmet',
         'gatsby-source-data',
         'gatsby-transformer-remark',
+        "gatsby-plugin-netlify",
         {
             resolve: 'gatsby-source-filesystem',
             options: {
