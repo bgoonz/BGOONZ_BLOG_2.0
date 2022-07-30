027512019e633734a7aed237a63010e6a941ee07
Bryan C Guner <bryan.guner@gmail.com>
2022-07-30 12:35:52 +0000

    update

---
+++ scripts/python-scripts-master/_UPDATE/Word-generator/isEng.py
@@ -0,0 +1,16 @@
+def getDict(file):
+    words = set()
+    with open(file) as d:
+        for w in d.read().split("\n"):
+            words.add(w.lower())
+    return words
+
+
+def isEng(word):
+    englishWords = getDict("dictionary.txt")
+    if word in englishWords:
+        return True
+    return False
+
+
+print(isEng("boot"))
