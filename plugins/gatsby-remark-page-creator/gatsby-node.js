160f120efcaf8371e834fd3ebd7e65a7540bec93
Bryan C Guner <bryan.guner@gmail.com>
2022-08-01 18:52:57 +0000

    hi

--- plugins/gatsby-remark-page-creator/gatsby-node.js
+++ plugins/gatsby-remark-page-creator/gatsby-node.js
@@ -1,15 +1,10 @@
 const path = require("path");
 const fs = require("fs");
-const {
-    createFilePath
-} = require("gatsby-source-filesystem");
+const { createFilePath } = require("gatsby-source-filesystem");
 const _ = require("lodash");
 
 
-function findFileNode({
-    node,
-    getNode
-}) {
+function findFileNode({ node, getNode }) {
     let fileNode = node;
     let ids = [fileNode.id];
 
@@ -33,24 +28,15 @@ function findFileNode({
         return null;
     }
 
-    return fileNode;
+    return fileNode
 }
 
-exports.onCreateNode = ({
-    node,
-    getNode,
-    actions
-}, options) => {
+exports.onCreateNode = ({ node, getNode, actions }, options) => {
 
-    const {
-        createNodeField
-    } = actions;
+    const { createNodeField } = actions;
 
     if (node.internal.type === "MarkdownRemark") {
-        let fileNode = findFileNode({
-            node,
-            getNode
-        });
+        let fileNode = findFileNode({ node, getNode });
         if (!fileNode) {
             throw new Error('could not find parent File node for MarkdownRemark node: ' + node);
         }
@@ -61,65 +47,22 @@ exports.onCreateNode = ({
         } else if (_.get(options, 'uglyUrls', false)) {
             url = path.join(fileNode.relativeDirectory, fileNode.name + '.html');
         } else {
-            url = createFilePath({
-                node,
-                getNode
-            });
+            url = createFilePath({ node, getNode });
         }
 
-        createNodeField({
-            node,
-            name: "url",
-            value: url
-        });
-        createNodeField({
-            node,
-            name: "absolutePath",
-            value: fileNode.absolutePath
-        });
-        createNodeField({
-            node,
-            name: "relativePath",
-            value: fileNode.relativePath
-        });
-        createNodeField({
-            node,
-            name: "absoluteDir",
-            value: fileNode.dir
-        });
-        createNodeField({
-            node,
-            name: "relativeDir",
-            value: fileNode.relativeDirectory
-        });
-        createNodeField({
-            node,
-            name: "base",
-            value: fileNode.base
-        });
-        createNodeField({
-            node,
-            name: "ext",
-            value: fileNode.ext
-        });
-        createNodeField({
-            node,
-            name: "name",
-            value: fileNode.name
-        });
+        createNodeField({ node, name: "url", value: url });
+        createNodeField({ node, name: "absolutePath", value: fileNode.absolutePath });
+        createNodeField({ node, name: "relativePath", value: fileNode.relativePath });
+        createNodeField({ node, name: "absoluteDir", value: fileNode.dir });
+        createNodeField({ node, name: "relativeDir", value: fileNode.relativeDirectory });
+        createNodeField({ node, name: "base", value: fileNode.base });
+        createNodeField({ node, name: "ext", value: fileNode.ext });
+        createNodeField({ node, name: "name", value: fileNode.name });
     }
 };
 
-exports.createPages = ({
-    graphql,
-    getNode,
-    actions,
-    getNodesByType
-}) => {
-    const {
-        createPage,
-        deletePage
-    } = actions;
+exports.createPages = ({ graphql, getNode, actions, getNodesByType }) => {
+    const { createPage, deletePage } = actions;
 
     // Use GraphQL to bring only the "id" and "html" (added by gatsby-transformer-remark)
     // properties of the MarkdownRemark nodes. Don't bring additional fields
@@ -143,9 +86,7 @@ exports.createPages = ({
             return Promise.reject(result.errors);
         }
 
-        const nodes = result.data.allMarkdownRemark.edges.map(({
-            node
-        }) => node);
+        const nodes = result.data.allMarkdownRemark.edges.map(({ node }) => node);
         const siteNode = getNode('Site');
         const siteDataNode = getNode('SiteData');
         const sitePageNodes = getNodesByType('SitePage');
