762ad0820161c8e95039aab0062b5203dab93195
Bryan C Guner <bryan.guner@gmail.com>
2022-08-01 23:24:02 +0000

    update

---
+++ static/js/searchbar.js
@@ -0,0 +1,14 @@
+// 'use strict';
+
+// !(function () {
+//     var t =
+//         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZDM2MTc3YmNjM2JjNmJiZjk3NzQyYSIsInRva2VuVHlwZSI6InB1YmxpYyIsImlhdCI6MTY1Mzc4Njc4OCwiZXhwIjoxNDI2ODE4Njc4OH0.Sa1QtfZAc7V0M1LbrliXfGQEK4UkLBRkNjrALtra-_o';
+//     function e() {
+//         var e = document.createElement('script'),
+//             a = navigator.language || navigator.userLanguage;
+//         (e.type = 'text/javascript'), (e.async = !0), (e.src = 'https://api.searchbar.org/v1/widget/' + t + '/' + a);
+//         var n = document.getElementsByTagName('script')[0];
+//         n.parentNode.insertBefore(e, n);
+//     }
+//     'complete' === document.readyState ? e() : window.attachEvent ? window.attachEvent('onload', e) : window.addEventListener('load', e, !1);
+// })();
