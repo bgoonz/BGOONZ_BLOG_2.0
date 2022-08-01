64d2785135621e43bce724b586b6fedc0d4e693e
Bryan C Guner <bryan.guner@gmail.com>
2022-08-01 22:39:18 +0000

    update

--- static/assets/js/page-load.js
+++ static/assets/js/page-load.js
@@ -1,7 +1,7 @@
 window.onGatsbyRouteUpdate = function () {
-  window.addMainNavigationHandlers();
-  window.addAnnouncementHandlers();
-  window.addVideoEmbedsHandlers();
-  window.addDocsNavigationHandlers();
-  window.addPageNavLinks();
+    window.addMainNavigationHandlers();
+    window.addAnnouncementHandlers();
+    window.addVideoEmbedsHandlers();
+    window.addDocsNavigationHandlers();
+    window.addPageNavLinks();
 };
