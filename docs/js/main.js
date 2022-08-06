8d5aaa7b5ed7558db051d438e7d715b724b28cc3
Bryan C Guner <bryan.guner@gmail.com>
2022-08-06 00:51:10 +0000

    update

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,15 +1,13 @@
-/* This is a jQuery function that is used to add a class to the active link in the navigation. */
-/* This is a jQuery function that is used to add a class to the active link in the navigation. */
-$('ul li a').bind('click', function() {
-    $('ul li a').removeClass('active');
-    $(this).addClass('active');
+$('ul li a').bind('click', function () {
+  $('ul li a').removeClass('active');
+  $(this).addClass('active');
 });
 
-$(document).ready(function() {
-    $.fn.waypoint.offset = 200;
-    $('article').waypoint(function(event, direction) {
-        var id = $(this).attr('id');
-        $('ul li a').removeClass('active');
-        $('ul li a[href="#' + id + '"]').addClass('active');
-    });
+$(document).ready(function () {
+  $.fn.waypoint.offset = 200;
+  $('article').waypoint(function (event, direction) {
+    var id = $(this).attr('id');
+    $('ul li a').removeClass('active');
+    $('ul li a[href="#' + id + '"]').addClass('active');
+  });
 });
