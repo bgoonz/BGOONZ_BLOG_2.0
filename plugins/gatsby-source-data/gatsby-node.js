c0bda17b95f1d07c83f8fd0d30bc449c210e1533
Bryan C Guner <bryan.guner@gmail.com>
2022-08-10 14:42:35 -0400

    Add files via upload

--- plugins/gatsby-source-data/gatsby-node.js
+++ plugins/gatsby-source-data/gatsby-node.js
@@ -1,3 +1,7 @@
+/**
+ * It reads all the files in the data directory, parses them, and creates a single node with the data
+ * @param changedFile - The file that was changed.
+ */
 const path = require('path');
 const yaml = require('js-yaml');
 const fse = require('fs-extra');
@@ -8,7 +12,7 @@ const _ = require('lodash');
 const metadataFileName = 'site-metadata.json';
 
 const parsers = {
-    yaml: (data) => yaml.safeLoad(data, { schema: yaml.JSON_SCHEMA }),
+    yaml: (data) => yaml.safeLoad(data, {schema: yaml.JSON_SCHEMA}),
     json: (data) => JSON.parse(data)
 };
 
@@ -84,7 +88,7 @@ async function readDirRecursively(dir, options) {
         const filePath = path.join(dir, file);
         const stats = await fse.stat(filePath);
         if (stats.isDirectory()) {
-            return readDirRecursively(filePath, { rootDir });
+            return readDirRecursively(filePath, {rootDir});
         } else if (stats.isFile()) {
             return path.relative(rootDir, filePath);
         } else {
