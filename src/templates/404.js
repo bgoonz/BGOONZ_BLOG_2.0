a3a51a67514a411209c5d5ee8e645edd404531ad
Bryan C Guner <bryan.guner@gmail.com>
2022-08-11 18:57:06 +0000

    Aug 11, 2022, 6:57 PM

--- src/templates/404.js
+++ src/templates/404.js
@@ -25,34 +25,30 @@ const codeStyles = {
 }
 
 // markup
-const NotFoundPage = () => {
-    return (
-        <main style={pageStyles}>
-            <title>Not found</title>
-            <h1 style={headingStyles}>Page not found</h1>
-            <p style={paragraphStyles}>
-                Sorry{" "}
-                <span role="img" aria-label="Pensive emoji">
-                    😔
-                </span>
-                {" "}
-                we couldn’t find what you were looking for.
-                <br/> {
-                process.env.NODE_ENV === "development" ? (
-                    <>
-                        <br/>
-                        Try creating a page in
-                        <code style={codeStyles}>src/pages/</code>.
-                        <br/>
-                    </>
-                ) : null
-            }
+const NotFoundPage = () => <main style={pageStyles}>
+    <title>Not found</title>
+    <h1 style={headingStyles}>Page not found</h1>
+    <p style={paragraphStyles}>
+        Sorry{" "}
+        <span role="img" aria-label="Pensive emoji">
+            😔
+        </span>
+        {" "}
+        we couldn’t find what you were looking for.
+        <br/> {
+        process.env.NODE_ENV === "development" ? (
+            <>
                 <br/>
-                <Link to="/">Go home</Link>.
-            </p>
-        </main>
-    )
-}
+                Try creating a page in
+                <code style={codeStyles}>src/pages/</code>.
+                <br/>
+            </>
+        ) : null
+    }
+        <br/>
+        <Link to="/">Go home</Link>.
+    </p>
+</main>
 
 export default NotFoundPage
 
